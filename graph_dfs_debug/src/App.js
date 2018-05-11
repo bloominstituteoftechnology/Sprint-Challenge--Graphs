c6e11ee28683781695679e8188663a06afe38fc5
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-05-11 12:28:13 -0400

    Bug fix for for drawing/dfs not yielding correct components

--- graph_dfs_debug/src/App.js
+++ graph_dfs_debug/src/App.js
@@ -35,7 +35,6 @@ class GraphView extends Component {
    * Draw the given verts
    */
   drawVerts(vertexes, color='blue', clear=true) {
-    console.log(this.props.graph)
     let canvas = this.refs.canvas;
     let ctx = canvas.getContext('2d');
     
@@ -51,7 +50,7 @@ class GraphView extends Component {
 
     for (let v of vertexes) { // From this vert
       for (let e of v.edges) { // To all these verts
-        const v2 = e.destination;
+        let v2 = e.destination;
         ctx.beginPath();
         ctx.moveTo(v.pos.x, v.pos.y);
         ctx.lineTo(v2.pos.x, v2.pos.y);
