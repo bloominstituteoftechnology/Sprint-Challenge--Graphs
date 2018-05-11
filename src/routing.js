5ff7f7cabdef6964e3a7f8777815da7f7ae8e1fb
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-05-11 12:16:24 -0500

    Comment Code
    - in routing.js:
    - comment code succintly yet thoroughly

--- src/routing.js
+++ src/routing.js
@@ -16,48 +16,48 @@ class Graph {
   constructor() {
     this.vertexes = [];
   }
-
+  // finds if a vertex exists and returns the value, else returns null
   findVertex(value) {
-    let found = this.vertexes.find(val => {
-      return val.value === value;
+    let found = this.vertexes.find(val => { 
+      return val.value === value; // returns value that matches query value
     });
     if (!found) {
-      found = null;
+      found = null; // if not found, assigns from undefined to null
     }
-    return found;
+    return found; // return result
   }
-
+  // bfs implementation
   bfs(start) {
-    const queue = [];
-    for (let v of this.vertexes) {
-      v.color = 'white';
-      v.parent = null;
+    const queue = []; // initialize the queue array
+    for (let v of this.vertexes) { // iterate over each vertex in graph
+      v.color = 'white'; // assigns color to white to indicate it has not been visited
+      v.parent = null; // initializes parent key on object
     }
-    start.color = 'gray';
-    queue.push(start);
-    while (queue.length > 0) {
-      const currentVert = queue[0];
-      for (let edge of currentVert.edges) {
-        let next = edge.destination
-        if (next.color === 'white') {
-          next.color = 'gray';
-          next.parent = currentVert;
-          queue.push(next);
+    start.color = 'gray'; // assigns active vertex to be gray per pseudocode instruction...could be omitted 
+    queue.push(start); // pushes start value into the queue to begin operation
+    while (queue.length > 0) { // checks if queue is empty and loops otherwise
+      const currentVert = queue[0]; // assigns current as index zero following FIFO
+      for (let edge of currentVert.edges) { // iterates over edges of vertex
+        let next = edge.destination // assigns next to be the destination of the vertex
+        if (next.color === 'white') { // checks if vertex has been visited
+          next.color = 'gray'; // change to working color if so
+          next.parent = currentVert; // assigns parent key of next as the current vertex
+          queue.push(next); // pushes the next vertex to the queue
         }
       }
-      currentVert.color = 'black';
-      queue.shift();
+      currentVert.color = 'black'; // sets visited color to black
+      queue.shift(); // shifts current off of the queue
     }
   }
-
+  // outputs results to console
   outputRoute(start) {
-    let route = [];
-    while (start) {
-      route = [...route].concat(start.value);
-      start = start.parent;
+    let route = []; // initialize an array to hold the route
+    while (start) { // runs while additional backwards travel path exists
+      route = [...route].concat(start.value); // assigns route to old route plus new vertex moving backwards
+      start = start.parent; // reassigns start, will be checked in next potential loop cycle
     }
-    const finalOutput = route.join(' --> ');
-    console.log(finalOutput);
+    const finalOutput = route.join(' --> '); // join array with arrow as specified in pseudocode
+    console.log(finalOutput); // console log the output
   }
 
   /**
