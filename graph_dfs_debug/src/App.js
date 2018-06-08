2b191d55fcf2398e5897d2de24c44c8c3ab761ce
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-06-08 12:18:15 -0400

    Stone Chris | Sprint Graphs

--- graph_dfs_debug/src/App.js
+++ graph_dfs_debug/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 // Define the size of the random graph
 const xCount = 4;
-const yCount = 3;
-const boxSize = 150;
+const yCount = 4;
+const boxSize = 154;
 const probability = 0.6;
 
 // Figure out the canvas size
@@ -59,7 +59,7 @@ class GraphView extends Component {
     }
 
     // Draw the verts on top
-    ctx.fillStyle = '#77f'; 
+    ctx.fillStyle = 'color'; 
 
     for (let v of vertexes) {
       ctx.beginPath();
@@ -155,7 +155,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.Button}>Random</button>
+        <button onClick={this.onButton}>Random</button>
         <GraphView graph={this.state.graph}></GraphView>
       </div>
     );
