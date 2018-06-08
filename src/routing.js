a8f40568b8c2ac8d809d2a1283f48434a1f48d0d
Carlos Andrade <carlos.andrade86@outlook.com>
2018-06-08 14:38:32 -0400

    Completed

--- src/routing.js
+++ src/routing.js
@@ -3,151 +3,108 @@
 /**
  * Edge class
  */
-class Edge {
-  constructor(destination, weight=1) {
-    this.destination = destination;
-    this.weight = weight;
-  }
-}
+import React, { Component } from 'react';
+import { Graph } from './graph';
+import './App.css';
+
+// !!! IMPLEMENT ME
+const canvasWidth = 400;
+const canvasHeight = 300;
+
+const vertexRadius = 10;
 
 /**
- * Vertex class
+ * GraphView
  */
-class Vertex {
-  constructor(value='vertex') {
-    this.value = value;
-    this.edges = [];
-  }
-}
-
-/**
- * Graph class
- */
-class Graph {
-
+class GraphView extends Component {
   /**
-   * Constructor
+   * On mount
    */
-  constructor() {
-    this.vertexes = [];
+  componentDidMount() {
+    this.updateCanvas();
   }
 
   /**
-   * This function looks through all the vertexes in the graph and returns the
-   * first one it finds that matches the value parameter.
-   *
-   * Used from the main code to look up the verts passed in on the command
-   * line.
-   *
-   * @param {*} value The value of the Vertex to find
-   *
-   * @return null if not found.
+   * On state update
    */
-  findVertex(value) {
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  /**
+   * Render the canvas
+   */
+  updateCanvas() {
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    
+    // Clear it
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.font = "30px Arial";
+    ctx.fillText('Hello World', 10, 50);
+
+    // console.log('in update canvas, vertex data is: ', this.props.graph);
+
+    for(let vertex of this.props.graph.vertexes) {
+      console.log('vertex names', vertex.value);
+      ctx.beginPath();
+      ctx.arc(vertex.pos.x, vertex.pos.y, vertexRadius, 0, 2 * Math.PI);
+      ctx.fillStyle = 'green';
+      ctx.fill();
+      ctx.strokeStyle = 'black'; //TODO: Optimize, many calls for stroke and fill style.
+      ctx.stroke();
+
+      ctx.fillStyle = 'black';
+      ctx.font = "10px Arial";
+      ctx.textAlign = "center";
+      ctx.textBaseLine = "middle";
+      ctx.fillText(vertex.value, vertex.pos.x, vertex.pos.y - vertexRadius);
+    }
+
     // !!! IMPLEMENT ME
+    // compute connected components
+    // draw edges
+    // draw verts
+    // draw vert values (labels)
   }
-
+  
   /**
-   * Breadth-First search from a starting vertex. This should keep parent
-   * pointers back from neighbors to their parent.
-   *
-   * @param {Vertex} start The starting vertex for the BFS
+   * Render
    */
-  bfs(start) {
-    // !!! IMPLEMENT ME
-  }
-
-  /**
-   * Print out the route from the start vert back along the parent
-   * pointers (set in the previous BFS)
-   *
-   * @param {Vertex} start The starting vertex to follow parent
-   *                       pointers from
-   */
-  outputRoute(start) {
-    // !!! IMPLEMENT ME
-  }
-
-  /**
-   * Show the route from a starting vert to an ending vert.
-   */
-  route(start, end) {
-    // Do BFS and build parent pointer tree
-    this.bfs(end);
-
-    // Show the route from the start
-    this.outputRoute(start);
+  render() {
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
   }
 }
+
 
 /**
- * Helper function to add bidirectional edges
+ * App
  */
-function addEdge(v0, v1) {
-  v0.edges.push(new Edge(v1));
-  v1.edges.push(new Edge(v0));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      graph: new Graph()
+    };
+
+    // !!! IMPLEMENT ME
+    // use the graph randomize() method
+    this.state.graph.debugCreateTestData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <GraphView graph={this.state.graph}></GraphView>
+      </div>
+    );
+  }
 }
 
-/**
- * Main
- */
-
-// Test for valid command line
-const args = process.argv.slice(2);
-
-if (args.length != 2) {
-  console.error('usage: routing hostA hostB');
-  process.exit(1);
-}
-
-// Build the entire Internet
-// (it's only a model)
-const graph = new Graph();
-const vertA = new Vertex('HostA');
-const vertB = new Vertex('HostB');
-const vertC = new Vertex('HostC');
-const vertD = new Vertex('HostD');
-const vertE = new Vertex('HostE');
-const vertF = new Vertex('HostF');
-const vertG = new Vertex('HostG');
-const vertH = new Vertex('HostH');
-
-addEdge(vertA, vertB);
-addEdge(vertB, vertD);
-addEdge(vertA, vertC);
-addEdge(vertC, vertD);
-addEdge(vertC, vertF);
-addEdge(vertG, vertF);
-addEdge(vertE, vertF);
-addEdge(vertH, vertF);
-addEdge(vertH, vertE);
-
-graph.vertexes.push(vertA);
-graph.vertexes.push(vertB);
-graph.vertexes.push(vertC);
-graph.vertexes.push(vertD);
-graph.vertexes.push(vertE);
-graph.vertexes.push(vertF);
-graph.vertexes.push(vertG);
-graph.vertexes.push(vertH);
-
-// Look up the hosts passed on the command line by name to see if we can
-// find them.
-
-const hostAVert = graph.findVertex(args[0]);
-
-if (hostAVert === null) {
-  console.error('routing: could not find host: ' + args[0]);
-  process.exit(2);
-}
-
-const hostBVert = graph.findVertex(args[1]);
-
-if (hostBVert === null) {
-  console.error('routing: could not find host: ' + args[1]);
-  process.exit(2);
-}
-
+export default App;
 // Show the route from one host to another
 
 graph.route(hostAVert, hostBVert);
