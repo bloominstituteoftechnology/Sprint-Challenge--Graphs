2a1696e407975b7c6a4b24a0780badf90187ece0
David Alexander Morales <damorales89@gmail.com>
2018-07-13 14:16:11 -0400

    more bugs

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,7 @@
 ## The only changes I made to the debug portion of the Sprint involved the button. `this.Button` was misspelled and therefore didn't work.
+
+## Changed some of the sizing for the graph display to make it all fit and not go outside the border of the box.
+
+## Changed the color value on the ctx.fill so that it would represent more of the colors implemented throughout the code.
+
+## Corrected a height to width error in the Render section.
--- graph_dfs_debug/src/App.js
+++ graph_dfs_debug/src/App.js
@@ -3,10 +3,10 @@ import { Graph } from './graph';
 import './App.css';
 
 // Define the size of the random graph
-const xCount = 4;
-const yCount = 3;
-const boxSize = 150;
-const probability = 0.6;
+const xCount = 5;
+const yCount = 5;
+const boxSize = 120;
+const probability = 0.7;
 
 // Figure out the canvas size
 const canvasWidth = boxSize * xCount;
@@ -59,7 +59,7 @@ class GraphView extends Component {
     }
 
     // Draw the verts on top
-    ctx.fillStyle = '#77f'; 
+    ctx.fillStyle = color; 
 
     for (let v of vertexes) {
       ctx.beginPath();
@@ -119,7 +119,7 @@ class GraphView extends Component {
    * Render
    */
   render() {
-    return <canvas ref="canvas" width={canvasHeight} height={canvasHeight}></canvas>;
+    return <canvas ref="canvas" width={canvasWidth} height={canvasHeight}></canvas>;
   }
 }
 
