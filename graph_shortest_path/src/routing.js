b31f4ec1b40e992a48ab5c57c8f2894f07e3599f
Axhon <axhonruiz@gmail.com>
2018-07-13 06:04:50 -1000

    findvertex

--- graph_shortest_path/src/routing.js
+++ graph_shortest_path/src/routing.js
@@ -4,9 +4,9 @@
  * Edge class
  */
 class Edge {
-  constructor(destination, weight=1) {
-    this.destination = destination;
-    this.weight = weight;
+  constructor(destination, weight = 1) {
+    this.destination = destination
+    this.weight = weight
   }
 }
 
@@ -14,9 +14,9 @@ class Edge {
  * Vertex class
  */
 class Vertex {
-  constructor(value='vertex') {
-    this.value = value;
-    this.edges = [];
+  constructor(value = 'vertex') {
+    this.value = value
+    this.edges = []
   }
 }
 
@@ -24,12 +24,11 @@ class Vertex {
  * Graph class
  */
 class Graph {
-
   /**
    * Constructor
    */
   constructor() {
-    this.vertexes = [];
+    this.vertexes = []
   }
 
   /**
@@ -45,6 +44,7 @@ class Graph {
    */
   findVertex(value) {
     // !!! IMPLEMENT ME
+    return this.vertexes.find(vert => vert.value === value)
   }
 
   /**
@@ -73,10 +73,10 @@ class Graph {
    */
   route(start, end) {
     // Do BFS and build parent pointer tree
-    this.bfs(end);
+    this.bfs(end)
 
     // Show the route from the start
-    this.outputRoute(start);
+    this.outputRoute(start)
   }
 }
 
@@ -84,8 +84,8 @@ class Graph {
  * Helper function to add bidirectional edges
  */
 function addEdge(v0, v1) {
-  v0.edges.push(new Edge(v1));
-  v1.edges.push(new Edge(v0));
+  v0.edges.push(new Edge(v1))
+  v1.edges.push(new Edge(v0))
 }
 
 /**
@@ -93,61 +93,61 @@ function addEdge(v0, v1) {
  */
 
 // Test for valid command line
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 
 if (args.length != 2) {
-  console.error('usage: routing hostA hostB');
-  process.exit(1);
+  console.error('usage: routing hostA hostB')
+  process.exit(1)
 }
 
 // Build the entire Internet
 // (it's only a model)
-const graph = new Graph();
-const vertA = new Vertex('HostA');
-const vertB = new Vertex('HostB');
-const vertC = new Vertex('HostC');
-const vertD = new Vertex('HostD');
-const vertE = new Vertex('HostE');
-const vertF = new Vertex('HostF');
-const vertG = new Vertex('HostG');
-const vertH = new Vertex('HostH');
+const graph = new Graph()
+const vertA = new Vertex('HostA')
+const vertB = new Vertex('HostB')
+const vertC = new Vertex('HostC')
+const vertD = new Vertex('HostD')
+const vertE = new Vertex('HostE')
+const vertF = new Vertex('HostF')
+const vertG = new Vertex('HostG')
+const vertH = new Vertex('HostH')
 
-addEdge(vertA, vertB);
-addEdge(vertB, vertD);
-addEdge(vertA, vertC);
-addEdge(vertC, vertD);
-addEdge(vertC, vertF);
-addEdge(vertG, vertF);
-addEdge(vertE, vertF);
-addEdge(vertH, vertF);
-addEdge(vertH, vertE);
+addEdge(vertA, vertB)
+addEdge(vertB, vertD)
+addEdge(vertA, vertC)
+addEdge(vertC, vertD)
+addEdge(vertC, vertF)
+addEdge(vertG, vertF)
+addEdge(vertE, vertF)
+addEdge(vertH, vertF)
+addEdge(vertH, vertE)
 
-graph.vertexes.push(vertA);
-graph.vertexes.push(vertB);
-graph.vertexes.push(vertC);
-graph.vertexes.push(vertD);
-graph.vertexes.push(vertE);
-graph.vertexes.push(vertF);
-graph.vertexes.push(vertG);
-graph.vertexes.push(vertH);
+graph.vertexes.push(vertA)
+graph.vertexes.push(vertB)
+graph.vertexes.push(vertC)
+graph.vertexes.push(vertD)
+graph.vertexes.push(vertE)
+graph.vertexes.push(vertF)
+graph.vertexes.push(vertG)
+graph.vertexes.push(vertH)
 
 // Look up the hosts passed on the command line by name to see if we can
 // find them.
 
-const hostAVert = graph.findVertex(args[0]);
+const hostAVert = graph.findVertex(args[0])
 
 if (hostAVert === null) {
-  console.error('routing: could not find host: ' + args[0]);
-  process.exit(2);
+  console.error('routing: could not find host: ' + args[0])
+  process.exit(2)
 }
 
-const hostBVert = graph.findVertex(args[1]);
+const hostBVert = graph.findVertex(args[1])
 
 if (hostBVert === null) {
-  console.error('routing: could not find host: ' + args[1]);
-  process.exit(2);
+  console.error('routing: could not find host: ' + args[1])
+  process.exit(2)
 }
 
 // Show the route from one host to another
 
-graph.route(hostAVert, hostBVert);
+graph.route(hostAVert, hostBVert)
