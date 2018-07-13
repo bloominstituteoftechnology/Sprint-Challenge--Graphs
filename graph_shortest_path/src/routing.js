810ac5cb3de0faa40c2ebd13c3ff9acc9294f833
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-07-13 10:58:40 -0700

    Routing.js - Implemented bfs(start) functionality

--- graph_shortest_path/src/routing.js
+++ graph_shortest_path/src/routing.js
@@ -61,7 +61,28 @@ class Graph {
    */
   bfs(start) {
     // !!! IMPLEMENT ME
-    
+    let queue = [];
+    let found = [];
+
+    queue.push(start);
+    found.push(start);
+
+    while (queue.length) {
+    const current = queue.shift();
+  
+      for (let edge of current.edges) {
+        let isNew = true;
+        found.forEach(x => {
+          if (x.value === edge.destination.value)
+              isNew = false;
+        })
+        if (isNew) {
+          edge.destination.origin = current;
+          queue.push(edge.destination);
+          found.push(edge.destination);
+        }
+      }
+    }
   }
 
   /**
