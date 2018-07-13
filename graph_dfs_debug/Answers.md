43fb58e3394eb3cf87cdc3ad8979a33bd8fc42e3
sadrak105 <sadrak10@gmail.com>
2018-07-13 13:22:35 -0400

    cleanup for graph

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,5 +1,6 @@
 Describe the fixes you made to the Graph implementation here.
 
 1. The button name did not match the funtion name given.
-2. Passed props.graph to the vertexes in the loops. As well the colors and fills.
+2. Passed props.graph to the vertexes in the loops.
 3. Changed the graph's defined space and radius.
+4. Removed last call of shift(), no need for it.
--- graph_dfs_debug/src/App.js
+++ graph_dfs_debug/src/App.js
@@ -45,8 +45,8 @@ class GraphView extends Component {
     }
 
     // Draw the edges
-    // ctx.lineWidth = 2;
-    // ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = color;
 
     for (let v of this.props.graph.vertexes) { // From this vert
       for (let e of v.edges) { // To all these verts
@@ -54,32 +54,26 @@ class GraphView extends Component {
         ctx.beginPath();
         ctx.moveTo(v.pos.x, v.pos.y);
         ctx.lineTo(v2.pos.x, v2.pos.y);
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = color;
         ctx.stroke();
       }
     }
 
     // Draw the verts on top
-    // ctx.fillStyle = '#77f'; 
+    ctx.fillStyle = '#77f';
 
     for (let v of this.props.graph.vertexes) {
       ctx.beginPath();
       ctx.arc(v.pos.x, v.pos.y, radius, 0, 2 * Math.PI, false);
       ctx.stroke();
-      ctx.fillStyle = '#77f';
       ctx.fill();
     }
 
     // Draw the vert names
-    // ctx.font = '10px sans-serif';
-    // ctx.textAlign = 'center';
-    // ctx.fillStyle = 'white';
+    ctx.font = '10px sans-serif';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'white';
 
     for (let v of this.props.graph.vertexes) {
-      ctx.font = '10px sans-serif';
-      ctx.textAlign = 'center';
-      ctx.fillStyle = 'white';
       ctx.fillText(v.value, v.pos.x, v.pos.y + 4);
     }
   }
