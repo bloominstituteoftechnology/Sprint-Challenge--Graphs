49cf3032111d029e6bac423ec65068ff86b21a1d
sadrak105 <sadrak10@gmail.com>
2018-07-13 13:04:36 -0400

    removed shift and changes

--- graph_dfs_debug/src/App.js
+++ graph_dfs_debug/src/App.js
@@ -45,35 +45,41 @@ class GraphView extends Component {
     }
 
     // Draw the edges
-    ctx.lineWidth = 2;
-    ctx.strokeStyle = color;
+    // ctx.lineWidth = 2;
+    // ctx.strokeStyle = color;
 
-    for (let v of vertexes) { // From this vert
+    for (let v of this.props.graph.vertexes) { // From this vert
       for (let e of v.edges) { // To all these verts
         const v2 = e.destination;
         ctx.beginPath();
         ctx.moveTo(v.pos.x, v.pos.y);
         ctx.lineTo(v2.pos.x, v2.pos.y);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = color;
         ctx.stroke();
       }
     }
 
     // Draw the verts on top
-    ctx.fillStyle = '#77f'; 
+    // ctx.fillStyle = '#77f'; 
 
-    for (let v of vertexes) {
+    for (let v of this.props.graph.vertexes) {
       ctx.beginPath();
       ctx.arc(v.pos.x, v.pos.y, radius, 0, 2 * Math.PI, false);
       ctx.stroke();
+      ctx.fillStyle = '#77f';
       ctx.fill();
     }
 
     // Draw the vert names
-    ctx.font = '10px sans-serif';
-    ctx.textAlign = 'center';
-    ctx.fillStyle = 'white';
+    // ctx.font = '10px sans-serif';
+    // ctx.textAlign = 'center';
+    // ctx.fillStyle = 'white';
 
-    for (let v of vertexes) {
+    for (let v of this.props.graph.vertexes) {
+      ctx.font = '10px sans-serif';
+      ctx.textAlign = 'center';
+      ctx.fillStyle = 'white';
       ctx.fillText(v.value, v.pos.x, v.pos.y + 4);
     }
   }
@@ -92,7 +98,7 @@ class GraphView extends Component {
    */
   updateCanvasConnectedComponents() {
     function randomHexColor() {
-      let color = ((Math.random() * 240)|0).toString(16);
+      let color = ((Math.random() * 240) | 0).toString(16);
 
       if (color.length === 1) {
         color = '0' + color; // leading zero for values less than 0x10
