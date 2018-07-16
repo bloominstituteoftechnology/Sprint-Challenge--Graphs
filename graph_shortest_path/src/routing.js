f336eb84c3f636b7c74d5e65d84a83203bca9c72
nickbrenn <nbren@tutanota.com>
2018-07-16 13:45:48 -0400

    Finished part 2

--- graph_shortest_path/src/routing.js
+++ graph_shortest_path/src/routing.js
@@ -4,7 +4,7 @@
  * Edge class
  */
 class Edge {
-  constructor(destination, weight=1) {
+  constructor(destination, weight = 1) {
     this.destination = destination;
     this.weight = weight;
   }
@@ -14,7 +14,7 @@ class Edge {
  * Vertex class
  */
 class Vertex {
-  constructor(value='vertex') {
+  constructor(value = "vertex") {
     this.value = value;
     this.edges = [];
   }
@@ -24,7 +24,6 @@ class Vertex {
  * Graph class
  */
 class Graph {
-
   /**
    * Constructor
    */
@@ -44,7 +43,12 @@ class Graph {
    * @return null if not found.
    */
   findVertex(value) {
-    // !!! IMPLEMENT ME
+    for (let v of this.vertexes) {
+      if (v.value === value) {
+        return v;
+      }
+    }
+    return null;
   }
 
   /**
@@ -53,8 +57,33 @@ class Graph {
    *
    * @param {Vertex} start The starting vertex for the BFS
    */
-  bfs(start) {
-    // !!! IMPLEMENT ME
+  bfs(start, end) {
+    let queue = [];
+    for (let v of this.vertexes) {
+      v.color = "white";
+      v.parent = null; // <-- Add parent initialization
+    }
+    start.color = "gray";
+    queue.push(start);
+
+    while (queue.length > 0) {
+      const u = queue[0];
+      for (let v of u.edges) {
+        if (v.destination.value === end.value) {
+          queue.unshift(start);
+          queue.pop();
+          return queue;
+        }
+        if (v.destination.color === "white") {
+          v.destination.color = "gray";
+          v.destination.parent = u; // <-- Keep a parent link
+          queue.push(v.destination);
+        }
+      }
+      queue.shift();
+      u.color = "black";
+    }
+    return null;
   }
 
   /**
@@ -64,19 +93,29 @@ class Graph {
    * @param {Vertex} start The starting vertex to follow parent
    *                       pointers from
    */
-  outputRoute(start) {
-    // !!! IMPLEMENT ME
+  outputRoute(route, end) {
+    let resultString = "";
+    for (let v of route) {
+      resultString += v.value + " --> ";
+    }
+    resultString += end.value;
+
+    console.log(resultString);
   }
 
   /**
    * Show the route from a starting vert to an ending vert.
    */
   route(start, end) {
+    if (start === end) {
+      console.log(start.value);
+      return;
+    }
     // Do BFS and build parent pointer tree
-    this.bfs(end);
+    const route = this.bfs(start, end);
 
     // Show the route from the start
-    this.outputRoute(start);
+    this.outputRoute(route, end);
   }
 }
 
@@ -96,21 +135,21 @@ function addEdge(v0, v1) {
 const args = process.argv.slice(2);
 
 if (args.length != 2) {
-  console.error('usage: routing hostA hostB');
+  console.error("usage: routing hostA hostB");
   process.exit(1);
 }
 
 // Build the entire Internet
 // (it's only a model)
 const graph = new Graph();
-const vertA = new Vertex('HostA');
-const vertB = new Vertex('HostB');
-const vertC = new Vertex('HostC');
-const vertD = new Vertex('HostD');
-const vertE = new Vertex('HostE');
-const vertF = new Vertex('HostF');
-const vertG = new Vertex('HostG');
-const vertH = new Vertex('HostH');
+const vertA = new Vertex("HostA");
+const vertB = new Vertex("HostB");
+const vertC = new Vertex("HostC");
+const vertD = new Vertex("HostD");
+const vertE = new Vertex("HostE");
+const vertF = new Vertex("HostF");
+const vertG = new Vertex("HostG");
+const vertH = new Vertex("HostH");
 
 addEdge(vertA, vertB);
 addEdge(vertB, vertD);
@@ -137,14 +176,14 @@ graph.vertexes.push(vertH);
 const hostAVert = graph.findVertex(args[0]);
 
 if (hostAVert === null) {
-  console.error('routing: could not find host: ' + args[0]);
+  console.error("routing: could not find host: " + args[0]);
   process.exit(2);
 }
 
 const hostBVert = graph.findVertex(args[1]);
 
 if (hostBVert === null) {
-  console.error('routing: could not find host: ' + args[1]);
+  console.error("routing: could not find host: " + args[1]);
   process.exit(2);
 }
 
