1b90f97134ccf551f730b22d2edcdf52b7791734
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-07-27 11:56:06 -0400

    Vertices are connected now but all they have the same color even if they are not connected

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,47 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+
+Errors in:
+
+def dfs(self, start, target=None):
+
+#error - unused vaiable y, and set with initial value of x, should be empty
+# y = set(x)
+
+  y = set()
+
+#error - x is a list of vertices, should be the current vertex to compare with the target
+# if x == target:
+
+  if z == target:
+
+#error - we have to add to y the visited Vertex(z)
+            
+  y.add(z)
+
+#error - we add to the x queue all the vertices connected with this current vertex z excludind the ones already visited
+# x.extend(self.vertices[z])            
+            
+  x.extend(self.vertices[z]- y)
+
+
+Errors in:
+
+def add_edge(self, start, end, bidirectional=True):
+
+#error - edge has to be connected with a different Vertex no the same one
+# start connects with end
+# self.vertices[start].add(start)
+
+  self.vertices[start].add(end)
+
+#error - edge has to be connected with a different Vertex no the same one
+# end connects with start if is bidirectional
+# if bidirectional:
+#     self.vertices[end].add(end)
+  
+  if bidirectional:
+      self.vertices[end].add(start)
+
+
+
+
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,20 +19,34 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        #error - edge has to be connected with a different Vertex no the same one
+        # start connects with end
+        # self.vertices[start].add(start)
+        self.vertices[start].add(end)
+        #error - edge has to be connected with a different Vertex no the same one
+        # end connects with start if is bidirectional
+        # if bidirectional:
+        #     self.vertices[end].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
         x = []
         x.append(start)
-        y = set(x)
-
+        #error - unused vaiable y, and set with initial value of x, should be empty
+        # y = set(x)
+        y = set()
         while x:
             z = x.pop()
-            if x == target:
+            #error - x is a list of vertices, should be the current vertex to compare with the target
+            # if x == target:
+            if z == target:
                 break
-            x.extend(self.vertices[z])
+            #error - we have to add to y the visited Vertex(z)
+            y.add(z)
+            #error - we add to the x queue all the vertices connected with this current vertex z excludind the ones already visited
+            # x.extend(self.vertices[z])            
+            x.extend(self.vertices[z]- y)
 
         return x
 
