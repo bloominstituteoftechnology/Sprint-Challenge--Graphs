c3271cc8a4cb8b611778a9c31fe90c047ad21990
AmyShackles <amyshackles@gmail.com>
2018-07-27 11:02:11 -0700

    Modified bfs
    - Each neighbor vertex now has reference to its parent
    - It now outputs the route by following the parent pointers from the starting vert

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -42,7 +42,9 @@ class Graph:
         @return {Vertex} the found Vertex
         """
         # !!!! IMPLEMENT ME
-        pass
+        vert = [v for v in self.vertices if v.value == value]
+
+        return vert[0]
 
     def bfs(self, start):
         """
@@ -51,8 +53,18 @@ class Graph:
 
         @param {Vertex} start: The starting vertex
         """
-        # !!!! IMPLEMENT ME
-        pass
+        start.color = "gray"
+        queue = [start]
+        while queue:
+            current = queue.pop(0)
+
+            for edge in current.edges:
+                vertex = edge.destination
+                if vertex.color == "white":
+                    vertex.color = "gray"
+                    vertex.parent = current
+                    queue.append(vertex)
+            current.color = "black"
 
     def output_route(self, start):
         """
@@ -62,7 +74,17 @@ class Graph:
         @param {Vertex} start: The starting Vertex to follow and print
         """
         # !!!! IMPLEMENT ME
-        pass
+        vertex = start
+        output = ""
+
+        while vertex:
+            output += vertex.value
+            if vertex.parent:
+                output += " --> "
+
+            vertex = vertex.parent
+
+        print(output)
 
     def route(self, start, end):
         # BFS to build the parent reference tree
