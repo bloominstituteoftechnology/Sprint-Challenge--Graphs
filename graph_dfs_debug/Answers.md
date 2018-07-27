d81fa3f5560ecbfc7ed68943cc060a5cef62df06
derrick-mei <derrick.mei.168@gmail.com>
2018-07-27 10:39:43 -0500

    made changes to dps

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,28 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+
+### def depth_first_search
+
+1. If you wanted to you could add conditionals to inside the add_vertex and add_edge to filter out when they input variables that shouldn't be entered.
+2. In your add edge, you added end to end.  I changed to end to start
+3. Your variables aren't clearly labeled:
+    1. dfs => depth_first_search
+    2. x => stack
+    3. y => visted
+    4. z => current
+4. I made the starter code in dfs => stack = [start] so you don't have to append
+5. You don't have to initialize the x/stack in visted, bc it is added below, but it doesn't hurt bc it's a set
+6. In while loop:
+    1. I replaced variables with newer variable names
+        1. if *current* == target
+        2. *stack*.extend(self.vertices[*current*])
+7. When extending stack with (self.vertices[current]), you need to subtract the visted set so that you don't revisit the vertexes that you already went to and go on a infinite loop
+8. you should return visted and not the stack so that you know which vertex's were touched and the target was really not there.
+
+### def graph_rec
+
+1. asdf
+
+
+### find_components
+
+1. 
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -1,6 +1,8 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+
+
 class Vertex:
     def __init__(self, label, component=-1):
         self.label = str(label)
@@ -10,6 +12,8 @@ class Vertex:
         return 'Vertex: ' + self.label
 
     """Trying to make this Graph class work..."""
+
+
 class Graph:
     def __init__(self):
         self.vertices = {}
@@ -21,20 +25,19 @@ class Graph:
     def add_edge(self, start, end, bidirectional=True):
         self.vertices[start].add(start)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
-    def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+    def depth_first_search(self, start, target=None):
+        stack = [start]
+        visted = set()
 
-        while x:
-            z = x.pop()
-            if x == target:
+        while stack:
+            current = stack.pop()
+            if current == target:
                 break
-            x.extend(self.vertices[z])
+            stack.extend(self.vertices[current] - visted)
 
-        return x
+        return visted
 
     def graph_rec(self, start, target=None):
         x = set()
@@ -49,7 +52,7 @@ class Graph:
 
         for vertex in self.vertices:
             if vertex in visited:
-                reachable = self.dfs(vertex)
+                reachable = self.depth_first_search(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
                 current_component += 1
