f96c40ecc31ab5ee1d5b9761e77f0c65fb4c677a
07sebascode <sebastiannorena@gmail.com>
2018-07-27 12:09:52 -0400

    commit 1

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -24,23 +24,35 @@ class Graph:
             self.vertices[end].add(end)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        #x = [] changet to stack=[]
+        stack=[start]
+        #x.append(start)
+        #y = set(x) changed to visited=set()
+        visited=set()
 
-        while x:
-            z = x.pop()
-            if x == target:
+        #while x: changed to while stack:
+        while stack:
+            #z = x.pop() changed to current = stack.pop()
+            current = stack.pop()
+            #if x == target: changet to if current == target:
+            if current == target:
                 break
-            x.extend(self.vertices[z])
+            # added visited.add(current)
+            visited.add(current)
+            #x.extend(self.vertices[z]) changed to visited.add(current) changed to the following line
+            stack.extend(self.vertices[current] - visited)
+            
 
-        return x
+        #return x changed to return visited
+        return visited
 
     def graph_rec(self, start, target=None):
         x = set()
-        x.append(start)
+        #x.append(start) append does not work on a set so I changed it to x.add(start)
+        x.add(start)
         for v in self.vertices[start]:
-            graph_rec(v)
+            #graph_rec(v) changed to self.graph_rec(v)
+            self.graph_rec(v)
         return x
 
     def find_components(self):
