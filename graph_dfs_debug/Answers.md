e297b5e555350b1848417d7d3248e0889fdaeefe
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-07-27 09:22:42 -0700

    dfs add_edge and find_components fixed

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,16 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+
+add_edge
+
+Edges are not showing because start and end verticies are pointing themselves. I switched them so start is pointing end and vice-versa.
+
+DFS
+
+-Variables were poorly named. After naming them properly, I also notices that they were all mixed up. It wasnt returning the collections which was supposed to be called 'stack' so I return the 'visited' set which was originally called some letter.
+
+Also while loop didnt check if the curretn vertex had been visited or not so I added an if statement to check that (if vertex not in visited).
+
+find_components
+
+On line 45, it should be 'if vertex <<<not>>> in visited' so we can implement dfs on the vertex if it wasnt visited.
+
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,22 +19,24 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[start].add(end)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        stack = []
+        stack.append(start)
+        visited = set(stack)
 
-        while x:
-            z = x.pop()
-            if x == target:
-                break
-            x.extend(self.vertices[z])
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited
+                visited.add(vertex)
+                if vertex == target:
+                    break
+                stack.extend(self.vertices[vertex])
 
-        return x
+        return visited
 
     def graph_rec(self, start, target=None):
         x = set()
@@ -48,7 +50,7 @@ class Graph:
         current_component = 0
 
         for vertex in self.vertices:
-            if vertex in visited:
+            if vertex not in visited:
                 reachable = self.dfs(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
