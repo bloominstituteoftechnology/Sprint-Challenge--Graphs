631fa73662d5beac65e847db153028c2ed3b8187
Bee C <b.choeung@gmail.com>
2018-07-27 11:41:38 -0400

    adding changes to dfs and add_edge functions

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,12 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+
+in def add_edge:()
+self.vertices[start].add(start)
+changed to:
+self.vertices[start].add(end) -> i changed it to end because we are adding edges and it has to have a start and end.
+self.vertices[end].add(end) 
+changed to:
+self.vertices[end].add(start) -> i specified a start or else it won't know to add start.
+
+in def dfs():
+i changed all x variable names to stack because it makes more sence.  I also changed z to current and y to visited.
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -20,22 +20,22 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        stack = []
+        stack.append(start)
+        visited = set(stack)
 
-        while x:
-            z = x.pop()
-            if x == target:
+        while stack:
+            current = stack.pop()
+            if stack == target:
                 break
-            x.extend(self.vertices[z])
+            stack.extend(self.vertices[z])
 
-        return x
+        return stack
 
     def graph_rec(self, start, target=None):
         x = set()
