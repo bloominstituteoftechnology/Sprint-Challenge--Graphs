df764994f7620bd1f8532e2e91e2cefcb1a78133
jesuarva <jesuarva.developer@gmail.com>
2018-07-27 17:51:06 +0200

    FIX BUG: Potential infinit loop in graeph.add_vertx()
    ALSO: Refactored the variable names.

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,6 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+
+BUGS in graph.py:
+
+1.  Bad vairable names in dfs. Changed to something more rediable.
+2.  Potential infinity loop in dfs, it worth to check if a vertex was already visited. Changed.
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -29,34 +29,50 @@ class Graph:
             self.vertices[end].add(end)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
-
-        while x:
-            z = x.pop()
-            if x == target:
-                break
-            x.extend(self.vertices[z])
-
-        return x
-
-    def graph_rec(self, start, target=None):
-        x = set()
-        x.append(start)
-        for v in self.vertices[start]:
-            graph_rec(v)
-        return x
-
-    def find_components(self):
+        stack = []
+        stack.append(start)
         visited = set()
-        current_component = 0
 
-        for vertex in self.vertices:
-            if vertex in visited:
-                reachable = self.dfs(vertex)
-                for other_vertex in reachable:
-                    other_vertex.component = current_component
-                current_component += 1
-                visited.update(reachable)
-        self.components = current_component
+        while stack:
+            current = stack.pop()
+            if current not in visited:
+                visited.add(current)
+                if stack == target:
+                    break
+                stack.extend(self.vertices[current] - visited)
+
+        return visited
+
+    # def graph_rec(self, start, target=None):
+    #     x = set()
+    #     x.append(start)
+    #     for v in self.vertices[start]:
+    #         graph_rec(v)
+    #     return x
+
+    # def find_components(self):
+    #     visited = set()
+    #     current_component = 0
+
+    #     for vertex in self.vertices:
+    #         if vertex in visited:
+    #             reachable = self.dfs(vertex)
+    #             for other_vertex in reachable:
+    #                 other_vertex.component = current_component
+    #             current_component += 1
+    #             visited.update(reachable)
+    #     self.components = current_component
+
+
+_graph = Graph()  # Instantiate your graph
+_graph.add_vertex('0')
+_graph.add_vertex('1')
+_graph.add_vertex('2')
+_graph.add_vertex('3')
+_graph.add_vertex('4')
+_graph.add_vertex('5')
+_graph.add_vertex('6')
+_graph.add_vertex('7')
+_graph.add_edge('0', '1')
+_graph.add_edge('0', '3')
+print('\nGraph vertices: ', _graph.vertices)
