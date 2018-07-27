ea1916adf0223acb534c7f927d0ac28354b1bf0c
A.D.Faris <adfaris@gmail.com>
2018-07-27 14:05:31 -0400

    struggled with the second

--- graph_dfs_debug/draw.py
+++ graph_dfs_debug/draw.py
@@ -12,7 +12,7 @@ from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self, graph, title='Graph', width=100, height=100,
+    def __init__(self, graph, title='Graph', width=10, height=10,
                  show_axis=False, show_grid=False, circle_size=35,
                  draw_components=False):
         if not graph.vertices:
