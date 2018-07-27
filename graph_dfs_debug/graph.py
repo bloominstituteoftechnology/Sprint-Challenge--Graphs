da02d6bebe80c95429ee0f5fbe55a5f2d47e832e
Joanne Jordan <Joanne.k.m.jordan@gmail.com>
2018-07-27 09:43:18 -0700

    Update graph.py

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -1,54 +1,73 @@
+import warnings
+
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+
 class Vertex:
+    """Vertex class includes a label (automatically cast to a string) and a component attribute."""
     def __init__(self, label, component=-1):
         self.label = str(label)
         self.component = component
 
     def __repr__(self):
+        """Represents each vertex using its label"""
         return 'Vertex: ' + self.label
 
-    """Trying to make this Graph class work..."""
+
 class Graph:
+    """Graph class that uses a dictionary of the vertices as keys with values are the end vertices of edges associated with the key"""
     def __init__(self):
         self.vertices = {}
         self.components = 0
 
     def add_vertex(self, vertex, edges=()):
+        """Add a vertex that doesn't exist or overwrite a vertex that does"""
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: one or more edges points to a vertx not in graph.')
+            self.vertices[vertex] = set(edges)
+        if vertex in self.vertices:
+            warnings.warn('Warning: vertex already existed. Previous edges have been overwritten.')
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        """Add an edge between existing vertices"""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Error: One or more specified vertices are not in the graph.')
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        """Depth first search of the graph using looping method"""
+        stack = [start]
+        visited = set()
 
-        while x:
-            z = x.pop()
-            if x == target:
+        while stack:
+            current = stack.pop()
+            if current == target:
                 break
-            x.extend(self.vertices[z])
+            visited.add(current)
+            # Add possible (unvisited) vertices to queue
+            stack.extend(self.vertices[current] - visited)
 
-        return x
-
-    def graph_rec(self, start, target=None):
-        x = set()
-        x.append(start)
-        for v in self.vertices[start]:
-            graph_rec(v)
-        return x
+        return visited
+  
+    '''def dfs_recursive(self, start, target=None):
+    """Depth frist search of the graph using recursion"""
+    visited = set()
+    visited.append(start)
+    for v in self.vertices[start]:
+    graph_rec(v)
+    return x'''
 
     def find_components(self):
+        """Identify components of graph and update the component value of each vertex"""
         visited = set()
         current_component = 0
 
         for vertex in self.vertices:
-            if vertex in visited:
+            if vertex not in visited:
                 reachable = self.dfs(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
