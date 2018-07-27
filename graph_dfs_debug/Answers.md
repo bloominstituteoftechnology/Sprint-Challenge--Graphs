0cd0dca73c9be74b7c6c395906948ed40c8e2d36
shellysun <svanillasun@gmail.com>
2018-07-27 11:03:30 -0700

    not yet done

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,19 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+1. change the graph.py method defined:
+    instead of using two separate definition. I put these two together
+    def search(self, start, target=None, method='dfs'):
+    ...
+    along with changing the name on line#57 reachable = self.search(vertex)
+
+2: change the draw.py def _setup_graph_renderer:
+    self.vertex_list to self.vertex_keys
+    and follow by:
+    graph_renderer.node_renderer.data_source.add(
+            self.vertex_keys, 'index')
+    this is much easier, but yours not wrong
+    also need to change everything else if it has vertex_list to vertex_keys
+    
+3. change the graph_demo.py last line with this code instead of main()
+
+print('Expected arguments: num_vertices num_edges draw_components')
+print('Both numbers should be integers, draw_components should be 0/1')
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -16,40 +16,45 @@ class Graph:
         self.components = 0
 
     def add_vertex(self, vertex, edges=()):
+        """Add a new vertex, optionally with edges to other vertices."""
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        """Add a edge (default bidirectional) between two vertices."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Vertices to connect not in graph!')
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
-    def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+    def search(self, start, target=None, method='dfs'):
+        """Search the graph using BFS or DFS."""
+        quack = [start]  # Queue or stack, depending on method
+        pop_index = 0 if method == 'bfs' else -1
+        visited = set()
 
-        while x:
-            z = x.pop()
-            if x == target:
+        while quack:
+            current = quack.pop(pop_index)
+            if current == target:
                 break
-            x.extend(self.vertices[z])
+            visited.add(current)
+            # Add possible (unvisited) vertices to queue
+            quack.extend(self.vertices[current] - visited)
 
-        return x
-
-    def graph_rec(self, start, target=None):
-        x = set()
-        x.append(start)
-        for v in self.vertices[start]:
-            graph_rec(v)
-        return x
+        return visited
 
     def find_components(self):
+        """Identify components and update vertex component ids."""
         visited = set()
         current_component = 0
 
         for vertex in self.vertices:
             if vertex in visited:
-                reachable = self.dfs(vertex)
+                reachable = self.search(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
                 current_component += 1
