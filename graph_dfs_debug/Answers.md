2d33e42c20fb648f6ffe055b5ff2d60303ea34fa
Steve <shalomlandsman@gmail.com>
2018-07-27 12:11:42 -0400

    fix add edge

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,3 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+1. add check in add_edge for attempts to add edge to nonexistent vertices.
+2. fix code in add_edge to connect egde to desired vertex.
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,9 +19,11 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Error - vertices not in graph!')
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
         x = []
