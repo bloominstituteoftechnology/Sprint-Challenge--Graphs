bf666124d485f387b6d7bae8d34781563428917e
David <AquilaVirtual>
2018-07-27 09:07:07 -0700

    Fixed dfs and made changes to document

--- graph_dfs_debug/graph_demo.py
+++ graph_dfs_debug/graph_demo.py
@@ -28,7 +28,7 @@ def main(num_vertices=8, num_edges=8, draw_components=True):
 
 
 if __name__ == '__main__':
-    if len(argv) == 4:
+    if len(argv) == 3:
         NUM_VERTICES = int(argv[1])
         NUM_EDGES = int(argv[2])
         DRAW_COMPONENTS = bool(int(argv[3]))
