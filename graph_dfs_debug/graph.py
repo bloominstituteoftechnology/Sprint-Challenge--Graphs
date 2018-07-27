8b33588706b1f550ef0becf4d6b2b560401206a7
maribelcuales <maribelcuales@gmail.com>
2018-07-28 00:55:20 +0800

    initial commit and input answers to Answers.md

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,22 +19,27 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        stack = []
+        stack.append(start)
+        visited = set(stack)
 
-        while x:
-            z = x.pop()
-            if x == target:
-                break
-            x.extend(self.vertices[z])
+        while stack:
+            current = stack.pop()
 
-        return x
+            if current == target:
+                return target
+                # break
+
+            visited.add(current)
+
+            stack.extend(self.vertices[current] - visited)
+
+        return visited
 
     def graph_rec(self, start, target=None):
         x = set()
