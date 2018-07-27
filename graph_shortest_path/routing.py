218fe628039a05ecb575cbeb164bb3cf6823ebe2
Bee C <b.choeung@gmail.com>
2018-07-27 18:36:11 -0400

    commit corrected routing.py

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -1,83 +1,88 @@
-#/usr/bin/env python
 
 import sys
-
 
 # Edge class
 class Edge:
     def __init__(self, destination, weight=1):
         self.destination = destination
         self.weight = weight
-
-
-# Vertex class
+#Vertex class
 class Vertex:
-    def __init__(self, value='vertex', color='white', parent=None):
+    def __init__(self,value='vertex', color="white", parent=None):
         self.value = value
         self.edges = []
-        # Color of this vertex
-        # Used to mark vertices for the traversal algorithm (BFS or DFS)
         self.color = color
-        # Parent reference to keep track of the previous node in the
-        # graph when traversing through the graph
-        self.parent = parent
+        self.parent = parent 
+ #Graph class
 
-
-# Graph class
 class Graph:
     def __init__(self):
         self.vertices = []
+    
 
+    '''
+    * function looks through all the vertices in the graph and returns the
+    * first one it finds that matches the value parameter.
+    *
+    * Used from the main code to look up the verts passed in on the command
+    * line.
+    *
+    * @param: {*} value: The value of the Vertex to find
+    *
+    * @return None if not found.
+    * return {Vertex} the found vertex
+    * 
+    '''
     def find_vertex(self, value):
-        """
-        Looks through all the vertices in the graph instance and returns
-        the first vertex it finds that matches the `value` parameter.
-
-        Used in the `main` function to look up the vertices passed in
-        from the command line.
-
-        @param {*} value: The value of the Vertex to find
-
-        @return None if no such Vertex exists in the Graph.
-        @return {Vertex} the found Vertex
-        """
-        # !!!! IMPLEMENT ME
-    def find_vertex(self, value):
+        #!!! IMPLEMENT ME
         vert = [v for v in self.vertices if v.value == value]
         
         return vert[0]
+        
 
+    '''
+    * Breadth-First search from a starting vertex. self should keep parent
+    * pointers back from neighbors to their parent.
+    *
+    * @param:Vertex start The starting vertex for the BFS
+    '''
     def bfs(self, start):
-        """
-        Breadth-First search from an input starting Vertex
-        Should maintain parent references back from neighbors to their parent.
-
-        @param {Vertex} start: The starting vertex
-        """
-        # !!!! IMPLEMENT ME
+        """Search the graph using BFS or DFS."""
         start.color = 'gray'
         queue = [start]
 
+        #init func already doing this
+        """
+        for vertex in self.vertices:
+            vertex.color = 'white'
+            vertex.parent = None
+        """
+        # refactor
+        """
+        start.color = 'gray'
+        queue.append(start)
+        """
         while queue:
             current = queue.pop(0)
 
-        for edge in current.edges:
-            vertex = edge.destination
-            if vertex.color ==  'white':
-                vertex.color = 'gray'
-                vertex.parent = current
-                queue.append(vertex)
+            for edge in current.edges:
+                vertex = edge.destination
+                if vertex.color ==  'white':
+                    vertex.color = 'gray'
+                    vertex.parent = current
+                    queue.append(vertex)
 
-        current.color = 'black'
+            current.color = 'black'
 
+    '''
+    * Print out the route from the start vert back along the parent
+    * pointers(self,set in the previous BFS)
+    *
+    * @param:Vertex start The starting vertex to follow parent
+    * pointers from
+    '''
     def output_route(self, start):
-        """
-        Print out the route from the start vertex back along its parent
-        references (these were set in the `bfs` method)
-
-        @param {Vertex} start: The starting Vertex to follow and print
-        """
-        # !!!! IMPLEMENT ME
+    #!!! IMPLEMENT ME
         vertex = start
         output = ''
 
@@ -90,12 +95,16 @@ class Graph:
         
         print(output)
     
-
+    # Show the route from a starting vert to an ending vert.
+    
     def route(self, start, end):
-        # BFS to build the parent reference tree
+        #Do BFS and build parent pointer tree
         self.bfs(end)
-        # print the route from the start Vertex
+
+        #Show the route from the start
         self.output_route(start)
+
+
 
 
 # Helper function to add bidirectional edges
