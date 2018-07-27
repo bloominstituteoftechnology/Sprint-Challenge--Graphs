1b9c773703b55c1e51f907d46c4e7bc41a61b584
jesuarva <jesuarva.developer@gmail.com>
2018-07-27 19:15:23 +0200

    REFACTOR draw.py a little

--- graph_dfs_debug/draw.py
+++ graph_dfs_debug/draw.py
@@ -6,15 +6,20 @@ from math import ceil, floor, sqrt
 from random import choice, random
 from bokeh.io import show, output_file
 from bokeh.plotting import figure
-from bokeh.models import (GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
-                          ColumnDataSource)
+from bokeh.models import (
+    GraphRenderer, StaticLayoutProvider, Circle, LabelSet,
+    ColumnDataSource
+)
 
 
 class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
-    def __init__(self, graph, title='Graph', width=100, height=100,
-                 show_axis=False, show_grid=False, circle_size=35,
-                 draw_components=False):
+
+    def __init__(
+        self, graph, title='Graph', width=100, height=100,
+        show_axis=False, show_grid=False, circle_size=35,
+        draw_components=False
+    ):
         if not graph.vertices:
             raise Exception('Graph should contain vertices!')
         self.graph = graph
@@ -22,12 +27,12 @@ class BokehGraph:
         self.height = height
         self.pos = {}  # dict to map vertices to x, y positions
         # Set up plot, the canvas/space to draw on
-        self.plot = figure(title=title, x_range=(0, width), y_range=(0, height))
+        self.plot = figure(title=title, x_range=(
+            0, width), y_range=(0, height))
         self.plot.axis.visible = show_axis
         self.plot.grid.visible = show_grid
         self._setup_graph_renderer(circle_size, draw_components)
         self._setup_labels()
-
 
     def _setup_graph_renderer(self, circle_size, draw_components):
         # The renderer will have the actual logic for drawing
@@ -38,8 +43,10 @@ class BokehGraph:
         # Add the vertex data as instructions for drawing nodes
         graph_renderer.node_renderer.data_source.add(
             [vertex.label for vertex in self.vertex_list], 'index')
-        colors = (self._get_connected_component_colors() if draw_components
-                  else self._get_random_colors())
+        colors = (
+            self._get_connected_component_colors() if draw_components
+            else self._get_random_colors()
+        )
         graph_renderer.node_renderer.data_source.add(colors, 'color')
         # And circles
         graph_renderer.node_renderer.glyph = Circle(size=circle_size,
@@ -82,9 +89,11 @@ class BokehGraph:
             label_data['y'].append(y_pos)
             label_data['names'].append(vertex_label)
         label_source = ColumnDataSource(label_data)
-        labels = LabelSet(x='x', y='y', text='names', level='glyph',
-                          text_align='center', text_baseline='middle',
-                          source=label_source, render_mode='canvas')
+        labels = LabelSet(
+            x='x', y='y', text='names', level='glyph',
+            text_align='center', text_baseline='middle',
+            source=label_source, render_mode='canvas'
+        )
         self.plot.add_layout(labels)
 
     def show(self, output_path='./graph.html'):
