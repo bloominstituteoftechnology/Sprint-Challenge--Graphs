2af2f982a7fa9c812e00094c32910495d1d5ca9c
07sebascode <sebastiannorena@gmail.com>
2018-07-27 14:00:48 -0400

    2nd part

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -14,14 +14,30 @@ class Graph:
     def __init__(self):
         self.vertices = {}
         self.components = 0
-
+"""
     def add_vertex(self, vertex, edges=()):
         self.vertices[vertex] = set(edges)
-
+    #should be like this:"""
+    def add_vertex(self, vertex, edges=()):
+        """Add a new vertex, optionally with edges to other vertices."""
+        if vertex in self.vertices:
+            raise Exception('Error: adding vertex that already exists')
+        if not set(edges).issubset(self.vertices):
+            raise Exception('Error: cannot have edge to nonexistent vertices')
+        self.vertices[vertex] = set(edges)
+"""
     def add_edge(self, start, end, bidirectional=True):
         self.vertices[start].add(start)
         if bidirectional:
             self.vertices[end].add(end)
+    #should be like this:"""
+    def add_edge(self, start, end, bidirectional=True):
+        """Add a edge (default bidirectional) between two vertices."""
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('Vertices to connect not in graph!')
+        self.vertices[start].add(end)
+        if bidirectional:
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
         #x = [] changet to stack=[]
@@ -51,17 +67,38 @@ class Graph:
         #x.append(start) append does not work on a set so I changed it to x.add(start)
         x.add(start)
         for v in self.vertices[start]:
-            #graph_rec(v) changed to self.graph_rec(v)
-            self.graph_rec(v)
+            if v not in x:
+                x.add(v)
+                #graph_rec(v) changed to self.graph_rec(v)
+                self.graph_rec(v)
         return x
 
-    def find_components(self):
+            
+            
+        
+
+"""    def find_components(self):
         visited = set()
         current_component = 0
 
         for vertex in self.vertices:
             if vertex in visited:
                 reachable = self.dfs(vertex)
+                for other_vertex in reachable:
+                    other_vertex.component = current_component
+                current_component += 1
+                visited.update(reachable)
+        self.components = current_component
+#should be like this""""
+
+def find_components(self):
+        """Identify components and update vertex component ids."""
+        visited = set()
+        current_component = 0
+
+        for vertex in self.vertices:
+            if vertex not in visited:
+                reachable = self.search(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
                 current_component += 1
