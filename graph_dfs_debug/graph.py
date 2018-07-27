e6cc8c105e22199907cfcfd115209dc2a23975ca
Usman Javed <ujaved931@gmail.com>
2018-07-27 21:19:12 +0500

    Added if/else statements to add_edge and add_vertex to fix some issues and raise errors

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -16,12 +16,16 @@ class Graph:
         self.components = 0
 
     def add_vertex(self, vertex, edges=()):
-        self.vertices[vertex] = set(edges)
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        if start not in self.vertices or end not in self.vertices:
+            raise Exception('raiseError vertices not found in graph')
+        else:
+            self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
         x = []
