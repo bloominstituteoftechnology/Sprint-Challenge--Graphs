9daddb1c54cf2aa7765aa36b06046d6299d33921
Usman Javed <ujaved931@gmail.com>
2018-09-04 01:54:41 +0500

    Fixed errors

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -45,32 +45,26 @@ class Graph:
         for vertex in self.vertices:
             if vertex.value == value:
                 return vertex
+        return None
 
     def bfs(self, start):
         """
         Breadth-First search from an input starting Vertex
         Should maintain parent references back from neighbors to their parent.
-
         @param {Vertex} start: The starting vertex
         """
-        # !!!! IMPLEMENT ME
-        for vertex in self.vertices:
-            vertex.color = 'white'
-            vertex.parent=  None
+        to_visit = [start]
 
-            start.color = 'grey'
-            queue = [start]
-            while queue:
-                current = queue.pop(0)
+        while to_visit:
+            now_vert = to_visit.pop(0)
+            for next_vert_edge in now_vert.edges:
+                next_vert = next_vert_edge.destination
+                next_parent = next_vert.parent
+                if next_parent is None and next_vert is not start:
+                    next_vert.parent = now_vert
+                    to_visit.append(next_vert)
 
-                for edge in current.edges:
-                    neighbor = edge.destination
-                    if neighbor.color == 'white':
-                        neight.color = 'grey'
-                        neight.parent = current
-                        queue.append(neighbor)
 
-                current.color = black
     def output_route(self, start):
         """
         Print out the route from the start vertex back along its parent
@@ -79,16 +73,12 @@ class Graph:
         @param {Vertex} start: The starting Vertex to follow and print
         """
         # !!!! IMPLEMENT ME
-        current = start
-        output = ''
-
-        while current is not None:
-            output += current.value
-            if current.parent:
-                output += '-->'
-            current = current.parent
-
-        print(output)
+        print()
+        vertex = start
+        while vertex is not None:
+            print(f'{vertex.value} --> ', end='')
+            vertex = vertex.parent
+        print('end-route')
         
 
     def route(self, start, end):
