4f4f908f0b8e3622a8254e032abfe8c20ac12ef3
unknown <alexisreyes90@gmail.com>
2018-09-08 12:47:57 -0400

    Still working on dfs, stuck for a bit, might move ahead to shortest path

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,29 +19,36 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        if start not in self.vertices and end not in self.vertices:
+            raise ValueError("No Entries.")
+
+        self.vertices[start].add(end) # start and start? its start node to end node to create the vertex
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
 
-        while x:
-            z = x.pop()
-            if x == target:
+        # DFS works with stacks
+
+        stack = [start] # the stack
+
+        visited = set() # visited node
+
+        while stack:
+            current = stack.pop() # why a z?
+            if current == target:
                 break
-            x.extend(self.vertices[z])
+            visited.add(current)
+            stack.extend(self.vertices[current])
 
-        return x
+        return visited
 
     def graph_rec(self, start, target=None):
         x = set()
-        x.append(start)
+        visited.append(start)
         for v in self.vertices[start]:
-            graph_rec(v)
-        return x
+            self.graph_rec(v)
+        return visited
 
     def find_components(self):
         visited = set()
