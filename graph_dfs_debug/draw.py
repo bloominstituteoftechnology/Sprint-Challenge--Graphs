85fa7e66a8463f468de541def1070e03082204de
JohnCoronel <john.m.coronel@gmail.com>
2018-09-08 20:25:43 -0700

    graph.py fixed

--- graph_dfs_debug/draw.py
+++ graph_dfs_debug/draw.py
@@ -14,7 +14,7 @@ class BokehGraph:
     """Class that takes a graph and exposes drawing methods."""
     def __init__(self, graph, title='Graph', width=100, height=100,
                  show_axis=False, show_grid=False, circle_size=35,
-                 draw_components=False):
+                 draw_components=True):
         if not graph.vertices:
             raise Exception('Graph should contain vertices!')
         self.graph = graph
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -15,26 +15,26 @@ class Graph:
         self.vertices = {}
         self.components = 0
 
+    def __repr__(self):
+        return str(self.vertices)
+
     def add_vertex(self, vertex, edges=()):
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
-
-        while x:
-            z = x.pop()
-            if x == target:
-                break
-            x.extend(self.vertices[z])
-
-        return x
+        stack = [start]
+        visited = set()
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+                stack.extend(self.vertices[vertex] - visited) 
+        return visited
 
     def graph_rec(self, start, target=None):
         x = set()
@@ -48,10 +48,29 @@ class Graph:
         current_component = 0
 
         for vertex in self.vertices:
-            if vertex in visited:
+            if vertex not in visited:
                 reachable = self.dfs(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
                 current_component += 1
                 visited.update(reachable)
         self.components = current_component
+
+graph = Graph()
+v1 = Vertex('John')
+v2 = Vertex('Steve')
+v3 = Vertex('Bob')
+v4 = Vertex('Jill')
+v5 = Vertex('Jane')
+graph.add_vertex(v1)
+graph.add_vertex(v2)
+graph.add_vertex(v3)
+graph.add_vertex(v4)
+graph.add_vertex(v5)
+graph.add_edge(v1,v2)
+graph.add_edge(v1,v3)
+graph.add_edge(v2,v3)
+graph.add_edge(v4,v5)
+graph.add_edge(v2,v5)
+
+print(graph)
