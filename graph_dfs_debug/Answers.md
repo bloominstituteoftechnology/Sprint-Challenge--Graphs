bf56774fdbb2dc58933c98aa3571056cd8cddafa
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-09-14 08:33:29 -0700

    starts answers and edits

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,5 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+The first pass of going through your class, going from top to bottom, here are some thoughts.
+
+- When adding a vertex, make sure the vertex being added doesn't already exist.
+- make sure the edges connect to vertices that do exist
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -1,6 +1,8 @@
 """
 Simple graph implementation compatible with BokehGraph class.
 """
+
+
 class Vertex:
     def __init__(self, label, component=-1):
         self.label = str(label)
@@ -10,12 +12,21 @@ class Vertex:
         return 'Vertex: ' + self.label
 
     """Trying to make this Graph class work..."""
+
+
 class Graph:
     def __init__(self):
         self.vertices = {}
         self.components = 0
 
     def add_vertex(self, vertex, edges=()):
+        # check to make sure vertex doesn't already exist
+        if vertex in self.vertices:
+            raise Exception(
+                'ERROR: The vertex {} already exists!'.format(vertex))
+        # check to make sure the vertices for the edges do exist
+        if not set(edges).issubset(self.vertices):
+            raise Exception("ERROR: One or more vertices don't exist")
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
@@ -55,3 +66,28 @@ class Graph:
                 current_component += 1
                 visited.update(reachable)
         self.components = current_component
+
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex(0)
+graph.add_vertex(1)
+graph.add_vertex(2)
+graph.add_vertex(3)
+graph.add_vertex(4)
+graph.add_vertex(5)
+graph.add_vertex(6)
+graph.add_vertex(7)
+graph.add_vertex(8)
+graph.add_vertex(9)
+
+graph.add_edge(0, 1)
+graph.add_edge(0, 3)
+graph.add_edge(1, 2)
+graph.add_edge(2, 5)
+graph.add_edge(2, 4)
+graph.add_edge(4, 9)
+graph.add_edge(3, 7)
+graph.add_edge(3, 6)
+graph.add_edge(7, 9)
+
+print(graph.vertices)
