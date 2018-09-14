9cc84e142293c4a6eb323a4e3b14fbb2c5f57f06
Simon <Simon_Freelance@hotmail.com>
2018-09-14 12:21:43 -0400

    Added a basic test for dfs

--- graph_dfs_debug/test_graph.py
+++ graph_dfs_debug/test_graph.py
@@ -43,6 +43,31 @@ class GraphTests(unittest.TestCase):
             vert3: set()
         })
 
+    def test_dfs(self):
+        vert0 = Vertex(0)
+        vert1 = Vertex(1)
+        vert2 = Vertex(2)
+        vert3 = Vertex(3)
+        vert4 = Vertex(4)
+        vert5 = Vertex(5)
+        self.graph.add_vertex(vert0)
+        self.graph.add_vertex(vert1)
+        self.graph.add_vertex(vert2)
+        self.graph.add_vertex(vert3)
+        self.graph.add_vertex(vert4)
+        self.graph.add_vertex(vert5)
+
+        self.graph.add_edge(vert0, vert1)
+        self.graph.add_edge(vert0, vert4)
+        self.graph.add_edge(vert0, vert2)
+        self.graph.add_edge(vert1, vert3)
+        self.graph.add_edge(vert5, vert4)
+        self.graph.add_edge(vert5, vert2)
+
+        print(self.graph.dfs(vert5, vert1))
+        # Does not reach Vertex 3 when 1 is found, where 1 is the target vertex
+        self.assertLess(len(self.graph.dfs(vert5, vert1)), 6)
+        
         
 
 if __name__ == '__main__':
