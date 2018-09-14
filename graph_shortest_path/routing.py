ff475aa20ceb10315edec141834a7c7d0267ef6c
Tyson Gushiken <tysonism@gmail.com>
2018-09-14 09:37:02 -0700

    Add code for find_vertex

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -41,8 +41,11 @@ class Graph:
         @return None if no such Vertex exists in the Graph.
         @return {Vertex} the found Vertex
         """
-        # !!!! IMPLEMENT ME
-        pass
+        for vertex in self.vertices:
+            if vertex.value == value:
+                return vertex
+
+        return None
 
     def bfs(self, start):
         """
