348ebcb936665024074443960a1828d21fa0c3d3
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-09-14 08:55:57 -0700

    add_edge dfs init fixed

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,22 +19,24 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        stack = []
+        stack.append(start)
+        visited = set()
 
-        while x:
-            z = x.pop()
-            if x == target:
+        while stack:
+            vertex = stack.pop()
+            if vertex not in visited:
+                visited.add(vertex)
+            if vertex == target:
                 break
-            x.extend(self.vertices[z])
+            stack.extend(self.vertices[vertex]-visited)
 
-        return x
+        return visited
 
     def graph_rec(self, start, target=None):
         x = set()
