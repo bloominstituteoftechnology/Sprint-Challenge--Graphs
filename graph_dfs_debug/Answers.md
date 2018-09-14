547da8f9065a2b74494ff91bf39734ee553b26eb
Vu Cao <cdhvu@yahoo.com>
2018-09-14 08:25:09 -0700

    Fix bug in add_edge and dfs method in graph.py

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,1 +1,16 @@
 Describe the fixes/improvements you made to the Graph implementation here.
+* Switch `start` and `end` argument for `add_edge` method in `graph.py`
+* Change `dfs` method to:
+```
+x = []  
+x.append(start)  
+y = set()  
+while x:  
+    z = x.pop(0)  
+    if z == target:  
+        break  
+    x.extend((i for i in self.vertices[z] if i not in y))  
+    y.add(z)  
+return y  
+```
+* 
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,22 +19,21 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
         x = []
         x.append(start)
-        y = set(x)
-
+        y = set()
         while x:
-            z = x.pop()
-            if x == target:
+            z = x.pop(0)
+            if z == target:
                 break
-            x.extend(self.vertices[z])
-
-        return x
+            x.extend((i for i in self.vertices[z] if i not in y))
+            y.add(z)
+        return y
 
     def graph_rec(self, start, target=None):
         x = set()
