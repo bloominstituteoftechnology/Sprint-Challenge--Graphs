1dc4bb97555fea0a47eb1a090fa7ebc941592ff9
Alec <afroenygam@gmail.com>
2018-09-14 14:03:30 -0400

    routing.py updated, not working perfectly

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -3,6 +3,20 @@
 import sys
 
 
+# Queue class
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 # Edge class
 class Edge:
     def __init__(self, destination, weight=1):
@@ -41,8 +55,13 @@ class Graph:
         @return None if no such Vertex exists in the Graph.
         @return {Vertex} the found Vertex
         """
-        # !!!! IMPLEMENT ME
-        pass
+        valueList = []
+        for vert in self.vertices:
+            valueList.append(vert.value)
+        if value in valueList:
+            return value
+        return None
+
 
     def bfs(self, start):
         """
@@ -51,8 +70,32 @@ class Graph:
 
         @param {Vertex} start: The starting vertex
         """
+        for vert in self.vertices:
+            vert.color = "white"
+            vert.parent = None
+        q = Queue()
+
+        start.color = "gray"
+        q.enqueue(start)
+        visited = []
+
+        while q.size() > 0:
+            v = q.dequeue()
+            #print('VVVVVV', v.value)
+            # if v not visited...
+            if v not in visited:
+                v.color = "gray"
+                if len(visited) > 0:
+                    v.parent = visited[-1]
+                    #print('PARENT!', v.parent.value)
+                else:
+                    v.parent = None
+                print("Vertex: ", v, v.color, v.parent)
+                visited.append(v) #mark vertex as visited
+                for next_vert in v.edges:
+
+                    q.enqueue(next_vert.destination)
         # !!!! IMPLEMENT ME
-        pass
 
     def output_route(self, start):
         """
@@ -62,7 +105,26 @@ class Graph:
         @param {Vertex} start: The starting Vertex to follow and print
         """
         # !!!! IMPLEMENT ME
-        pass
+        returnString = ''
+        returnString += start.value
+
+        if start.parent:
+            next_vert = start.parent
+            print('next_vert', next_vert)
+
+            while next_vert.parent:
+                returnString += ' --> '
+                returnString += next_vert.value
+                next_vert = next_vert.parent
+
+            return returnString
+
+        else:
+            return returnString
+
+
+
+
 
     def route(self, start, end):
         # BFS to build the parent reference tree
@@ -75,6 +137,43 @@ class Graph:
 def add_edge(start, end):
     start.edges.append(Edge(end))
     end.edges.append(Edge(start))
+
+print('hoo')
+graph = Graph()
+vertA = Vertex('HostA')
+vertB = Vertex('HostB')
+vertC = Vertex('HostC')
+vertD = Vertex('HostD')
+vertE = Vertex('HostE')
+vertF = Vertex('HostF')
+vertG = Vertex('HostG')
+vertH = Vertex('HostH')
+
+add_edge(vertA, vertB)
+add_edge(vertB, vertD)
+add_edge(vertA, vertC)
+add_edge(vertC, vertD)
+add_edge(vertC, vertF)
+add_edge(vertG, vertF)
+add_edge(vertE, vertF)
+add_edge(vertH, vertF)
+add_edge(vertH, vertE)
+
+graph.vertices.append(vertA)
+graph.vertices.append(vertB)
+graph.vertices.append(vertC)
+graph.vertices.append(vertD)
+graph.vertices.append(vertE)
+graph.vertices.append(vertF)
+graph.vertices.append(vertG)
+graph.vertices.append(vertH)
+
+print(graph.find_vertex(vertA).value)
+print('THEN THE BFS')
+print(graph.bfs(vertA))
+print("THEN THE OUTPUT")
+print(graph.output_route(vertH))
+print('ttttt')
 
 
 if __name__ == '__main__':
