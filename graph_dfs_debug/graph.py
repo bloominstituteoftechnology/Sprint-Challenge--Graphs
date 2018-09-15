e7db87ce1a8e4c0095173af5ba578b5130447b29
TangledTessellations <mcevoya@oregonstate.edu>
2018-09-15 13:10:31 -0700

    Revert all changes in graph.py

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -3,9 +3,8 @@ Simple graph implementation compatible with BokehGraph class.
 """
 class Vertex:
     def __init__(self, label, component=-1):
-        self.label = label
+        self.label = str(label)
         self.component = component
-        self.edges = set()
 
     def __repr__(self):
         return 'Vertex: ' + self.label
@@ -16,63 +15,43 @@ class Graph:
         self.vertices = {}
         self.components = 0
 
-    def add_vertex(self, vertex):
-        self.vertices[vertex] = Vertex(vertex)
+    def add_vertex(self, vertex, edges=()):
+        self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].edges.add(end)
+        self.vertices[start].add(start)
         if bidirectional:
-            self.vertices[end].edges.add(start)
+            self.vertices[end].add(end)
 
-    def dfs(self, start, target):  
-        # Uses self.vertices indexes (which are the same as each vertex's label) to find if value in graph
-        stack = []
-        stack.append(start)
-        visited = []
+    def dfs(self, start, target=None):
+        x = []
+        x.append(start)
+        y = set(x)
 
-        while stack:
-            current = stack.pop()
-            visited.append(current)
-            if current == target:
-                return True
-            for child in self.vertices[current].edges:
-                if child not in visited:
-                    stack.append(child)
+        while x:
+            z = x.pop()
+            if x == target:
+                break
+            x.extend(self.vertices[z])
 
-        return False
+        return x
 
-    def dfs_recursive(self, start, target=None, visited=[]):
-        visited.append(start)
-        if self.vertices[start].label == target:
-            return True
-        for child in self.vertices[start].edges:
-            if child not in visited:
-                return self.dfs_recursive(child, target, visited)
-        return False
-
-    def dft_stack(self, starting_vertex_id):
-        stack = []
-        stack.append(starting_vertex_id)
-        visited = []
-        while len(stack) > 0:
-            v = stack.pop()
-            if v not in visited:
-                visited.append(v) 
-                for next_vert in self.vertices[v].edges:
-                    stack.append(next_vert)
-        return visited
+    def graph_rec(self, start, target=None):
+        x = set()
+        x.append(start)
+        for v in self.vertices[start]:
+            graph_rec(v)
+        return x
 
     def find_components(self):
         visited = set()
         current_component = 0
 
         for vertex in self.vertices:
-            if vertex not in visited:                  # Changed to not in (we want to perform this operation on vertices we HAVENT visited)
-                reachable = self.dft_stack(vertex)
+            if vertex in visited:
+                reachable = self.dfs(vertex)
                 for other_vertex in reachable:
-                    self.vertices[other_vertex].component = current_component  
+                    other_vertex.component = current_component
                 current_component += 1
                 visited.update(reachable)
         self.components = current_component
-
-
