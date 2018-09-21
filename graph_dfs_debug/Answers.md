13c52dbe3bf5b9323b822a78445ed1f806ce3e4c
chainchompa <chainchompa@gmail.com>
2018-09-21 12:09:36 -0400

    Changed variable names to be more descriptive

--- graph_dfs_debug/Answers.md
+++ graph_dfs_debug/Answers.md
@@ -1,14 +1,20 @@
 Describe the fixes/improvements you made to the Graph implementation here.
 
 Issue 1: No edges connecting the vertices
--Fixed add_edge method by having the start vertex add the end vertex, and the end vertex
-add the start vertex
+-Fixed add_edge method by having the start vertex add the end vertex, and the end vertex add the start vertex
 
 Issue 2: All vertices are the same color
--
+-In find_components method it needed to check if was not in visited
 
 Issue 3: Adding a new vertex does not create an object from the Vertex class
 -
 
 Issue 4: Variables in dfs and graph_rec methods are not descriptive
--
+-Changed the following variable names
+dfs:
+x => stack
+y => visited
+z => current
+graph_rec:
+x => queue
+v => vertex
--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -24,31 +24,32 @@ class Graph:
             self.vertices[end].add(start)
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        stack = []
+        stack.append(start)
+        visited = set()
 
-        while x:
-            z = x.pop()
-            if x == target:
+        while stack:
+            current = stack.pop()
+            if current == target:
                 break
-            x.extend(self.vertices[z])
+            visited.add(current)
+            stack.extend(self.vertices[current] - visited)
 
-        return x
+        return visited
 
     def graph_rec(self, start, target=None):
-        x = set()
-        x.append(start)
-        for v in self.vertices[start]:
-            graph_rec(v)
-        return x
+        queue = set()
+        queue.add(start)
+        for vertex in self.vertices[start]:
+            self.graph_rec(vertex)
+        return queue
 
     def find_components(self):
         visited = set()
         current_component = 0
 
         for vertex in self.vertices:
-            if vertex in visited:
+            if vertex not in visited:
                 reachable = self.dfs(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
