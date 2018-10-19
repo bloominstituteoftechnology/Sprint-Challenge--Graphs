776ab83658997ce544e0a668e8c097f753960da4
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-10-19 11:15:26 -0400

    sprint wip

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -16,25 +16,41 @@ class Graph:
         self.components = 0
 
     def add_vertex(self, vertex, edges=()):
-        self.vertices[vertex] = set(edges)
+        if vertex not in self.vertices:
+            self.vertices[vertex] = set(edges)
+        else:
+            IndexError('This vertex already exists')
+        # self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
-        if bidirectional:
-            self.vertices[end].add(end)
+       if start in self.vertices and end in self.vertices:
+          self.vertices[start].add(end)
+          if bidirectional:
+            self.vertices[end].add(start)
+        else:
+            IndexError('Both ends of an edge must be in the list of vertices')
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        visited = []
+        stack = [start]
+        while len(stack) > 0:
+            current = stack.pop()
+            if current not in visited:
+                if current == target:
+                    break
+                visited.append(current)
+                
+        # x = []
+        # x.append(start)
+        # y = set(x)
 
-        while x:
-            z = x.pop()
-            if x == target:
-                break
-            x.extend(self.vertices[z])
+        # while x:
+        #     z = x.pop()
+        #     if x == target:
+        #         break
+        #     x.extend(self.vertices[z])
 
-        return x
+        # return x
 
     def graph_rec(self, start, target=None):
         x = set()
