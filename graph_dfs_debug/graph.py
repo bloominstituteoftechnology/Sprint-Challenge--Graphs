8e6070593a859629db288048a8a881162fd1d03e
bri79319 <bri79319@gmail.com>
2018-10-19 12:34:13 -0400

    Referencing code to implement graphs and fix errors

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -19,28 +19,32 @@ class Graph:
         self.vertices[vertex] = set(edges)
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
+        start.component += 1
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start)
+            end.component += 1
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        # x = []
+        # x.append(start)
+        # y = set(x)
+        stack = [start]
+        visited = set(stack)
 
-        while x:
-            z = x.pop()
-            if x == target:
+        while stack:
+            curr = stack.pop()
+            if curr == target:
                 break
-            x.extend(self.vertices[z])
-
-        return x
+            visited.add(curr)
+            stack.extend(self.vertices[curr] - visited)
+        return visited
 
     def graph_rec(self, start, target=None):
         x = set()
-        x.append(start)
+        x.add(start)
         for v in self.vertices[start]:
-            graph_rec(v)
+            self.graph_rec(v)
         return x
 
     def find_components(self):
@@ -48,7 +52,7 @@ class Graph:
         current_component = 0
 
         for vertex in self.vertices:
-            if vertex in visited:
+            if vertex not in visited:
                 reachable = self.dfs(vertex)
                 for other_vertex in reachable:
                     other_vertex.component = current_component
