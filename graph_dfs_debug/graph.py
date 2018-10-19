cfcc9e8edd12ba263b4dab3b1f1673e99677fdcc
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-10-19 11:06:13 -0500

    Almost finished with Graph.py
    I have connected them visually, but the color of all of them are still the same, I haven't been able to figure that out in my assignment either. I also finshed the Answers.md

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -2,9 +2,10 @@
 Simple graph implementation compatible with BokehGraph class.
 """
 class Vertex:
-    def __init__(self, label, component=-1):
+    def __init__(self, label, component=-1, color ='gray'):
         self.label = str(label)
         self.component = component
+        self.color = color
 
     def __repr__(self):
         return 'Vertex: ' + self.label
@@ -16,27 +17,36 @@ class Graph:
         self.components = 0
 
     def add_vertex(self, vertex, edges=()):
-        self.vertices[vertex] = set(edges)
+        if vertex not in self.vertices: #if there are vertex is not in verticies
+            self.vertices[vertex] = set(edges) #put the vertex in the vertices and set the edges
+        else:
+            IndexError("error")
 
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        if start in self.vertices and end in self.vertices: #if start of a vertex is in vertices and the end is as well then connect them
+            #do both end of the vertice, this is where bidirectional will come in.
+            #add the ends to each other start-end end-start
+            self.vertices[start].add(end) #change the start to end
         if bidirectional:
-            self.vertices[end].add(end)
+            self.vertices[end].add(start) #change the end to start
+        else:
+            IndexError("error")
 
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        visited = []    # create a empty visited dictionary
+        stack = [start] # create a stack dictionary where start is placed
+        while len(stack) > 0: # while stack is still holding start loop through it
+            # pop off each off the elements
+            destacked = stack.pop()
+            #check to see if any of the destacked elements have been visited
+            #and if any of the destacked elements are the target.
 
-        while x.size() > 0:
-            z = x.pop()
-            x.append(z)
-            print(z, 'z')
-            if x == target:
-                break
-            x.extend(self.vertices[z])
-
-        return x
+            if destacked not in visited:
+                if destacked == target:
+                    break
+                    # place all of the destacked elements into the visited dictionary
+                visited.append(destacked)
+       
 
     def graph_rec(self, start, target=None):
         x = set()
