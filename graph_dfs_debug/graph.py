e91e69bd306aeee50c2b43a7bf44c2b13fd5752e
Charlie Winslow <cwinslow22@yahoo.com>
2018-10-19 10:34:31 -0500

    Charlie Winslow
    initial commit

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -55,3 +55,12 @@ class Graph:
                 current_component += 1
                 visited.update(reachable)
         self.components = current_component
+
+graph = Graph()  # Instantiate your graph
+graph.add_vertex('0')
+graph.add_vertex('1')
+graph.add_vertex('2')
+graph.add_vertex('3')
+graph.add_edge('0', '1')
+graph.add_edge('0', '3')
+print(graph.vertices)
