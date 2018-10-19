4bc18c09983a7309db7eaf9b3e9827571cc781ac
mkerbleski <mkerbles@gmail.com>
2018-10-19 11:38:16 -0600

    complete basic path print from routing

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -30,11 +30,9 @@ class Graph:
 
     def find_vertex(self, value):
         """
-        Looks through all the vertices in the graph instance and returns
-        the first vertex it finds that matches the `value` parameter.
+        Looks through all the vertices in the graph instance and returns the first vertex it finds that matches the `value` parameter.
 
-        Used in the `main` function to look up the vertices passed in
-        from the command line.
+        Used in the `main` function to look up the vertices passed in from the command line.
 
         @param {*} value: The value of the Vertex to find
 
@@ -42,7 +40,13 @@ class Graph:
         @return {Vertex} the found Vertex
         """
         # !!!! IMPLEMENT ME
-        pass
+        
+        for vertex in self.vertices:
+            print('45', vertex.value)
+            print(value)
+            if vertex.value == value:
+                return vertex
+        return None
 
     def bfs(self, start):
         """
@@ -51,8 +55,35 @@ class Graph:
 
         @param {Vertex} start: The starting vertex
         """
+        print('bfs start')
+        q = []
+        q.append([self.vertices[0]])
+        visited = []
+        while q:
+            path = q.pop(0)
+            node = path[-1]
+            print('-------------------')
+            print('path', path)
+            print('node.value', node.value)
+            print('start.value', start.value)
+
+            if node.value == start.value:
+                print('final path', path)
+                return path
+                break
+            else: 
+                visited.append(node)
+                for edge in node.edges:
+                    if edge not in visited:
+                        print('edge', edge.destination)
+                        new_path = list(path)
+                        new_path.append(edge.destination)
+                        q.append(new_path)
+        print('false')
+        return False
+
         # !!!! IMPLEMENT ME
-        pass
+        # return path
 
     def output_route(self, start):
         """
@@ -62,13 +93,24 @@ class Graph:
         @param {Vertex} start: The starting Vertex to follow and print
         """
         # !!!! IMPLEMENT ME
-        pass
+        route = self.bfs(start)
+        print('route', route)
+        global sen
+        sen = []
+        for v in route:
+            print('v', v.value)
+            print('sen', sen)
+            sen.append(v.value)
+            print('sen', sen)
+        arr = ' --> '.join(sen)
+        print('arr', arr)
+        return sen
 
     def route(self, start, end):
         # BFS to build the parent reference tree
         self.bfs(end)
         # print the route from the start Vertex
-        self.output_route(start)
+        self.output_route(end)
 
 
 # Helper function to add bidirectional edges
@@ -114,12 +156,14 @@ if __name__ == '__main__':
     # Look up the hosts passed in from the command line by
     # name to see if we can find them.
     hostAVert = graph.find_vertex(sys.argv[1])
+    print(hostAVert)
 
     if hostAVert is None:
         print('routing.py: could not find host: ', sys.argv[1])
         sys.exit()
 
     hostBVert = graph.find_vertex(sys.argv[2])
+    print(hostBVert)
 
     if hostBVert is None:
         print('routing.py: could not find host: ', sys.argv[2])
