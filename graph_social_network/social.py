9cddc15563494907498b6cd1019fbdddeeb0ac9a
David Layman <davidlayman85@gmail.com>
2018-11-30 16:10:34 -0500

    has an error, and unfinished, but it's what Ive got right now

--- graph_social_network/social.py
+++ graph_social_network/social.py
@@ -11,7 +11,7 @@ class SocialGraph:
 		self.lastID = 0
 		self.users = {}
 		self.friendships = {}
-
+		
 	def addFriendship(self, userID, friendID):
 		if userID == friendID:
 			print("WARNING: You cannot be friends with yourself")
@@ -30,12 +30,47 @@ class SocialGraph:
 		self.lastID = 0
 		self.users = {}
 		self.friendships = {}
-		range = len(names)-1
-		while numUsers > 0:
-			self.addUser(names[random.randint(0,range)])
-			numUsers = numUsers - 1
+		ranges = len(names)-1
+		copy = numUsers
+		while copy > 0:
+			self.addUser(names[random.randint(0,ranges)])
+			copy = copy - 1
         # Create friendships
-		
+		copy = numUsers
+		friendNums = list()
+		high,low = avgFriendships,avgFriendships
+		if avgFriendships%2 == 1:
+			friendNums.append(avgFriendships)
+			copy = copy - 1
+		while low > 1:
+			low = low -1
+			high = high +1
+		i = 0
+		while copy/2 > i:
+			num = random.randint(low,high)
+			if num > avgFriendships:
+				friendNums.append(num)
+				friendNums.append(num-avgFriendships)
+			elif num == avgFriendships:
+				friendNums.append(num)
+				friendNums.append(num)
+			else:
+				friendNums.append(num)
+				friendNums.append(num+avgFriendships)
+			i = i + 1
+		length = len(self.users)
+		print(friendNums)
+		for j in range(1,length):
+			already_added = set()
+			for k in range(0,friendNums.pop(0)):
+				f = random.randint(1,length)
+				while f == j or f in already_added:
+						f = random.randint(1,length)
+				print("{}, {}".format(j,f))
+				self.addFriendship(j,f)
+				already_added.add(f)
+			
+			
 	def getAllSocialPaths(self, userID):
 		visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
