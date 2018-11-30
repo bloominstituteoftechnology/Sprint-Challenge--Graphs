2dc4ff741a5da3245698e3becdd4b5063aaffad6
Andrew Liao <andrewoliao@gmail.com>
2018-11-30 08:49:16 -0800

    Implement find_vertex()

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -41,8 +41,19 @@ class Graph:
         @return None if no such Vertex exists in the Graph.
         @return {Vertex} the found Vertex
         """
-        # !!!! IMPLEMENT ME
-        pass
+        
+        visited = set()
+        stack = [self.vertices[0]]
+        while len(stack) > 0:
+            vertex = stack.pop()
+            if vertex.value == value:
+                return vertex
+            visited.add(vertex)
+            # add neighbors to stack
+            for edge in vertex.edges:
+                if edge.destination not in visited:
+                    stack.append(edge.destination)
+        return None
 
     def bfs(self, start):
         """
@@ -77,53 +88,56 @@ def add_edge(start, end):
     end.edges.append(Edge(start))
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: routing.py hostA hostB')
-        sys.exit()
+# if __name__ == '__main__':
+#     if len(sys.argv) != 3:
+#         print('Usage: routing.py hostA hostB')
+#         sys.exit()
 
-    graph = Graph()
-    vertA = Vertex('HostA')
-    vertB = Vertex('HostB')
-    vertC = Vertex('HostC')
-    vertD = Vertex('HostD')
-    vertE = Vertex('HostE')
-    vertF = Vertex('HostF')
-    vertG = Vertex('HostG')
-    vertH = Vertex('HostH')
+graph = Graph()
+vertA = Vertex('HostA')
+vertB = Vertex('HostB')
+vertC = Vertex('HostC')
+vertD = Vertex('HostD')
+vertE = Vertex('HostE')
+vertF = Vertex('HostF')
+vertG = Vertex('HostG')
+vertH = Vertex('HostH')
 
-    add_edge(vertA, vertB)
-    add_edge(vertB, vertD)
-    add_edge(vertA, vertC)
-    add_edge(vertC, vertD)
-    add_edge(vertC, vertF)
-    add_edge(vertG, vertF)
-    add_edge(vertE, vertF)
-    add_edge(vertH, vertF)
-    add_edge(vertH, vertE)
+add_edge(vertA, vertB)
+add_edge(vertB, vertD)
+add_edge(vertA, vertC)
+add_edge(vertC, vertD)
+add_edge(vertC, vertF)
+add_edge(vertG, vertF)
+add_edge(vertE, vertF)
+add_edge(vertH, vertF)
+add_edge(vertH, vertE)
 
-    graph.vertices.append(vertA)
-    graph.vertices.append(vertB)
-    graph.vertices.append(vertC)
-    graph.vertices.append(vertD)
-    graph.vertices.append(vertE)
-    graph.vertices.append(vertF)
-    graph.vertices.append(vertG)
-    graph.vertices.append(vertH)
+graph.vertices.append(vertA)
+graph.vertices.append(vertB)
+graph.vertices.append(vertC)
+graph.vertices.append(vertD)
+graph.vertices.append(vertE)
+graph.vertices.append(vertF)
+graph.vertices.append(vertG)
+graph.vertices.append(vertH)
 
     # Look up the hosts passed in from the command line by
     # name to see if we can find them.
-    hostAVert = graph.find_vertex(sys.argv[1])
+    # hostAVert = graph.find_vertex(sys.argv[1])
 
-    if hostAVert is None:
-        print('routing.py: could not find host: ', sys.argv[1])
-        sys.exit()
 
-    hostBVert = graph.find_vertex(sys.argv[2])
+    # if hostAVert is None:
+    #     print('routing.py: could not find host: ', sys.argv[1])
+    #     sys.exit()
 
-    if hostBVert is None:
-        print('routing.py: could not find host: ', sys.argv[2])
-        sys.exit()
+    # hostBVert = graph.find_vertex(sys.argv[2])
 
-    # Show the route from one Vertex to the other
-    graph.route(hostAVert, hostBVert)
+    # if hostBVert is None:
+    #     print('routing.py: could not find host: ', sys.argv[2])
+    #     sys.exit()
+
+    # # Show the route from one Vertex to the other
+    # graph.route(hostAVert, hostBVert)
+
+print(graph.find_vertex("HostG").value)       
