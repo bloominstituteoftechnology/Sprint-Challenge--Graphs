246ec166ec78b6013e968dc9bdf9ed0a290065cb
Anthony Greb <agreb255@gmail.com>
2018-11-30 16:27:03 -0500

    social.py

--- graph_social_network/README.md
+++ graph_social_network/README.md
@@ -49,7 +49,7 @@ Note that in this sample, Users 3, 4 and 9 are not in User 1's extended social n
 
 1. To create 100 users with an average of 10 friends each, how many times would you need to call `addFriendship()`? Why? 500 times, because 10 x 100 = 1,000 / 2 = 500 (1pt)
 
-2. If you create 1000 users with an average of 5 random friends each, what percentage of other users will be in a particular user's extended social network? What is the average degree of separation between a user and those in his/her extended network? (2pts)
+2. If you create 1000 users with an average of 5 random friends each, what percentage of other users will be in a particular user's extended social network? What is the average degree of separation between a user and those in his/her extended network? it would be a little over 99% (2pts)
 
 ## 4. Stretch Goal
 
--- graph_social_network/social.py
+++ graph_social_network/social.py
@@ -2,6 +2,23 @@ import random
 import math
 
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if (self.size()) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -48,19 +65,22 @@ class SocialGraph:
         self.users = {}
         self.friendships = {}
         for i in range(0, numUsers):
+
             self.addUser(f"User {i}")
         possibleFriendships = []
+
         for userID in self.users:
+
             for friendID in range(userID + 1, self.lastID + 1):
+
                 possibleFriendships.append((userID, friendID))
+
         random.shuffle(possibleFriendships)
+
         for i in range(0, math.floor(numUsers * avgFriendships / 2)):
+            
             friendship = possibleFriendships[i]
             self.addFriendship(friendship[0], friendship[1])
-
-        # Add users
-
-        # Create friendships
 
     def getAllSocialPaths(self, userID):
         """
@@ -71,8 +91,25 @@ class SocialGraph:
 
         The key is the friend's ID and the value is the path.
         """
-        visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+        visited = {}
+
+        q = Queue()
+        q.enqueue([userID])
+
+        while q.size() > 0:
+            path = q.dequeue()
+            newUserID = path[-1]
+
+            if newUserID not in visited:
+                visited[newUserID] = path
+
+            for friendID in self.friendships[newUserID]:
+
+                if friendID not in visited:
+                    new_path = list(path)
+                    new_path.append(friendID)
+                    q.enqueue(new_path)
+
         return visited
 
 
