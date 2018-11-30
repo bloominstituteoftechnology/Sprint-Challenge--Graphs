2a56bc7074c73ed5d42dbbbb90a68444a2f372db
jaymzsocool <james.mcraejr@gmail.com>
2018-11-30 11:59:52 -0500

    shortest path

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -2,6 +2,18 @@
 
 import sys
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if (self.size()) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
 # Edge class
 class Edge:
@@ -42,7 +54,10 @@ class Graph:
         @return {Vertex} the found Vertex
         """
         # !!!! IMPLEMENT ME
-        pass
+        for vertex in self.vertices:
+            if vertex.value == value:
+                return vertex
+        return None
 
     def bfs(self, start):
         """
@@ -52,7 +67,17 @@ class Graph:
         @param {Vertex} start: The starting vertex
         """
         # !!!! IMPLEMENT ME
-        pass
+        queue = Queue()
+        start.color = "black"
+        queue.enqueue(start)
+        while queue.size() > 0:
+            vertex = queue.dequeue()
+            for edge in vertex.edges:
+                destination = edge.destination
+                if destination.color == "white":
+                    destination.color = "black"
+                    destination.parent = vertex
+                    queue.enqueue(destination)
 
     def output_route(self, start):
         """
@@ -62,7 +87,15 @@ class Graph:
         @param {Vertex} start: The starting Vertex to follow and print
         """
         # !!!! IMPLEMENT ME
-        pass
+        current = start
+        output_str = ""
+
+        while current is not None:
+            output_str += current.value
+            if current.parent is not None:
+                output_str += "-->"
+            current = current.parent
+        print(output_str)
 
     def route(self, start, end):
         # BFS to build the parent reference tree
@@ -110,6 +143,7 @@ if __name__ == '__main__':
     graph.vertices.append(vertF)
     graph.vertices.append(vertG)
     graph.vertices.append(vertH)
+    
 
     # Look up the hosts passed in from the command line by
     # name to see if we can find them.
