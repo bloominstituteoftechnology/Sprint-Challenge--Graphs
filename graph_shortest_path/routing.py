c6c95129002279caef465d3a22c0a53cc86732d6
Andrew Liao <andrewoliao@gmail.com>
2018-11-30 09:20:05 -0800

    Clean up personal testing code

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -107,67 +107,57 @@ def add_edge(start, end):
     end.edges.append(Edge(start))
 
 
-# if __name__ == '__main__':
-#     if len(sys.argv) != 3:
-#         print('Usage: routing.py hostA hostB')
-#         sys.exit()
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: routing.py hostA hostB')
+        sys.exit()
 
-graph = Graph()
-vertA = Vertex('HostA')
-vertB = Vertex('HostB')
-vertC = Vertex('HostC')
-vertD = Vertex('HostD')
-vertE = Vertex('HostE')
-vertF = Vertex('HostF')
-vertG = Vertex('HostG')
-vertH = Vertex('HostH')
+    graph = Graph()
+    vertA = Vertex('HostA')
+    vertB = Vertex('HostB')
+    vertC = Vertex('HostC')
+    vertD = Vertex('HostD')
+    vertE = Vertex('HostE')
+    vertF = Vertex('HostF')
+    vertG = Vertex('HostG')
+    vertH = Vertex('HostH')
 
-add_edge(vertA, vertB)
-add_edge(vertB, vertD)
-add_edge(vertA, vertC)
-add_edge(vertC, vertD)
-add_edge(vertC, vertF)
-add_edge(vertG, vertF)
-add_edge(vertE, vertF)
-add_edge(vertH, vertF)
-add_edge(vertH, vertE)
+    add_edge(vertA, vertB)
+    add_edge(vertB, vertD)
+    add_edge(vertA, vertC)
+    add_edge(vertC, vertD)
+    add_edge(vertC, vertF)
+    add_edge(vertG, vertF)
+    add_edge(vertE, vertF)
+    add_edge(vertH, vertF)
+    add_edge(vertH, vertE)
 
-graph.vertices.append(vertA)
-graph.vertices.append(vertB)
-graph.vertices.append(vertC)
-graph.vertices.append(vertD)
-graph.vertices.append(vertE)
-graph.vertices.append(vertF)
-graph.vertices.append(vertG)
-graph.vertices.append(vertH)
+    graph.vertices.append(vertA)
+    graph.vertices.append(vertB)
+    graph.vertices.append(vertC)
+    graph.vertices.append(vertD)
+    graph.vertices.append(vertE)
+    graph.vertices.append(vertF)
+    graph.vertices.append(vertG)
+    graph.vertices.append(vertH)
 
     # Look up the hosts passed in from the command line by
     # name to see if we can find them.
-    # hostAVert = graph.find_vertex(sys.argv[1])
+    hostAVert = graph.find_vertex(sys.argv[1])
 
 
-    # if hostAVert is None:
-    #     print('routing.py: could not find host: ', sys.argv[1])
-    #     sys.exit()
+    if hostAVert is None:
+        print('routing.py: could not find host: ', sys.argv[1])
+        sys.exit()
 
-    # hostBVert = graph.find_vertex(sys.argv[2])
+    hostBVert = graph.find_vertex(sys.argv[2])
 
-    # if hostBVert is None:
-    #     print('routing.py: could not find host: ', sys.argv[2])
-    #     sys.exit()
+    if hostBVert is None:
+        print('routing.py: could not find host: ', sys.argv[2])
+        sys.exit()
 
-    # # Show the route from one Vertex to the other
-    # graph.route(hostAVert, hostBVert)
+    # Show the route from one Vertex to the other
+    graph.route(hostAVert, hostBVert)
 
-
-# for v in graph.vertices:
-#     if v.parent:
-#         value = v.parent.value
-#     print(v.value, value)
-
-graph.route(vertA, vertD)
-graph.route(vertA, vertH)
-graph.route(vertA, vertA)
-graph.route(vertE, vertB)
 
 
