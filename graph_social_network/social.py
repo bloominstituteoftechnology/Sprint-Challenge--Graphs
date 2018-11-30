a01d4a2d530138774893203a8a149ec889514b58
Csillag61 <csillagok@gmail.com>
2018-11-30 13:04:29 -0500

    shortest path, bfs path

--- graph_social_network/social.py
+++ graph_social_network/social.py
@@ -101,7 +101,23 @@ class SocialGraph:
                     graph, start, graph[friend], visited)
         return visited
 
-    
+    def bfs_paths(self, graph, start, goal):
+        queue = [(start, [start])]
+        while queue:
+            (vertex, path) = queue.pop(0)
+            for next in graph[vertex] - set(path):
+                if next == goal:
+                    yield path + [next]
+                else:
+                    queue.append((next, path +[next]))
+
+    def shortest_path(self, graph, start, goal):
+        if(start == goal):
+            return [start]
+        try:
+            return next(self.bfs_paths(graph, start, goal))
+        except StopIteration:
+            return None
 
         
     
