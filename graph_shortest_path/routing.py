2a35e95bb577ad688c05b48cc19340d84ac4f9da
lyang9 <lyang9@buffalo.edu>
2018-12-02 23:52:11 -0500

    Completed find_vertex method

--- graph_shortest_path/routing.py
+++ graph_shortest_path/routing.py
@@ -3,6 +3,20 @@
 import sys
 
 
+class Queue:
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
+
 # Edge class
 class Edge:
     def __init__(self, destination, weight=1):
@@ -42,7 +56,10 @@ class Graph:
         @return {Vertex} the found Vertex
         """
         # !!!! IMPLEMENT ME
-        pass
+        for vertex in self.vertices:
+            if vertex.value == value:
+                return vertex
+        return None 
 
     def bfs(self, start):
         """
