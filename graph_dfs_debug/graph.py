68af914303da0bd267dfdccb7c759ced166b70df
Adrian Adames <adrianadames@gmail.com>
2018-12-22 21:25:02 -0500

    Fixed dfs via stack, dfs via recursion. Renamed variables for readability. Made comments to code for later use.

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -14,34 +14,46 @@ class Graph:
     def __init__(self):
         self.vertices = {}
         self.components = 0
-
     def add_vertex(self, vertex, edges=()):
         self.vertices[vertex] = set(edges)
-
     def add_edge(self, start, end, bidirectional=True):
-        self.vertices[start].add(start)
+        self.vertices[start].add(end)
         if bidirectional:
-            self.vertices[end].add(end)
-
+            self.vertices[end].add(start)
+    # def dfs(self, start, target=None):
+    #     x = []
+    #     x.append(start)
+    #     y = set(x)
+    #     while x:
+    #         z = x.pop()
+    #         if x == target:
+    #             break
+    #         x.extend(self.vertices[z])
+    #     return x
     def dfs(self, start, target=None):
-        x = []
-        x.append(start)
-        y = set(x)
+        x_stack = [] # to act as our stack
+        x_stack.append(start) # we append the start node to the stack
+        visited = set() # y will serve to hold all the visited vertices. we add visited nodes to visited in the while loop as they're popped from the stack
+        while x_stack: # while x (the stack) is not empty
+            currentVert = x_stack.pop() #we pop out the element at the end of x. z is set equal to x
+            if currentVert == target:
+                return currentVert #serves to break the loop and return the target vertex. As set up before with the break, the return at the bottom would just return the last popped element in the stack.
+            if currentVert not in visited:
+                visited.add(currentVert)
+                x_stack.extend(self.vertices[currentVert])
+            else:
+                pass
+        return None   #if the function loops through all the vertices and their edges without finding the target vertex, return None. 
 
-        while x:
-            z = x.pop()
-            if x == target:
-                break
-            x.extend(self.vertices[z])
-
-        return x
-
-    def graph_rec(self, start, target=None):
-        x = set()
-        x.append(start)
-        for v in self.vertices[start]:
-            graph_rec(v)
-        return x
+    def graph_rec(self, start, target=None, visited = set()): # we set the value of visited here. otherwise, it will continuously be reset to empty for each recursive call and the recursive call will infinitely loop.
+        # x.append(start) # changed x to visited for readability
+        visited.add(start) #set object doesn't have an append method
+        for vert in self.vertices[start]:
+            if vert in visited:
+                pass
+            else:
+                self.graph_rec(vert)
+        return visited
 
     def find_components(self):
         visited = set()
@@ -55,3 +67,40 @@ class Graph:
                 current_component += 1
                 visited.update(reachable)
         self.components = current_component
+
+# # TEST code
+# graph = Graph()
+# v0 = Vertex(0)
+# v1 = Vertex(1)
+# v2 = Vertex(2)
+# v3 = Vertex(3)
+# v4 = Vertex(4)
+# v5 = Vertex(5)
+# v6 = Vertex(6)
+# v7 = Vertex(7)
+# v8 = Vertex(8)
+# v9 = Vertex(9)
+
+# graph.add_vertex(v0)
+# graph.add_vertex(v1)
+# graph.add_vertex(v2)
+# graph.add_vertex(v3)
+# graph.add_vertex(v4)
+# graph.add_vertex(v5)
+# graph.add_vertex(v6)
+# graph.add_vertex(v7)
+# graph.add_vertex(v8)
+# graph.add_vertex(v9)
+
+# graph.add_edge(v0,v1)
+# graph.add_edge(v0,v3)
+# graph.add_edge(v1,v2)
+# graph.add_edge(v2,v5)
+# graph.add_edge(v2,v4)
+# graph.add_edge(v4,v9)
+# graph.add_edge(v3,v7)
+# graph.add_edge(v3,v6)
+# graph.add_edge(v7,v9)
+
+# # print(graph.dfs(v0,v8))
+# # print(graph.graph_rec(v0))
