22f976a3c2f04b3d7fc061461b48e5b2f5581209
Adrian Adames <adrianadames@gmail.com>
2018-12-22 22:52:02 -0500

    Fixed dfs methods again (issues related to the target and what the functions should return). Fixed the issue with disconnected vertices not having a different color from the connected vertices.

--- graph_dfs_debug/graph.py
+++ graph_dfs_debug/graph.py
@@ -24,34 +24,43 @@ class Graph:
     def dfs(self, start, target=None):
         x_stack = [] # to act as our stack
         x_stack.append(start) # we append the start node to the stack
-        visited = set() # y will serve to hold all the visited vertices. we add visited nodes to visited in the while loop as they're popped from the stack
+        # visited = set() # y will serve to hold all the visited vertices. we add visited nodes to visited in the while loop as they're popped from the stack
+        visited = [] #changed from set to array because a set is unordered. This is necessary if we want to see the path taken by the algorithm to reach the target. 
         while x_stack: # while x (the stack) is not empty
             currentVert = x_stack.pop() #we pop out the element at the end of x. z is set equal to x
+            if currentVert not in visited:
+                visited.append(currentVert)
+                x_stack.extend(self.vertices[currentVert])
             if currentVert == target:
                 # return currentVert #serves to break the loop and return the target vertex. As set up before with the break, the return at the bottom would just return the last popped element in the stack.
                 return visited #serves to break the loop and return all the vertices that were visited before finding the target vertex.
-            if currentVert not in visited:
-                visited.add(currentVert)
-                x_stack.extend(self.vertices[currentVert])
             else:
                 pass
-        return None   #if the function loops through all the vertices and their edges without finding the target vertex, return None. 
+        if target is None: #For cases when you want the full depth first traversal (i.e. when you don't input a target vertex)
+            return visited
+        else:
+            return None   #if the function loops through all the vertices and their edges without finding the target vertex, return None. 
 
-    def graph_rec(self, start, target=None, visited = set()): # we set the value of visited here. otherwise, it will continuously be reset to empty for each recursive call and the recursive call will infinitely loop.
+    def graph_rec(self, start, target=None, visited = []): #changed data type of visited from set to array to preserve order # we set the value of visited here. otherwise, it will continuously be reset to empty for each recursive call and the recursive call will infinitely loop.
         # x.append(start) # changed x to visited for readability
-        visited.add(start) #set object doesn't have an append method
-        print(start.label) 
+        visited.append(start) #set object doesn't have an append method
+        # print(start.label) 
         for vert in self.vertices[start]:
             if vert not in visited:
                 self.graph_rec(vert)
+        if target in visited or target is None:
+            return visited #can also make it so that this returns only the path leading to the target node.
+        else:
+            return None 
 
     def find_components(self):
         visited = set()
         current_component = 0
 
         for vertex in self.vertices:
+            visited.add(vertex) #vertices weren't added to the visited set here. 
             if vertex in visited:
-                reachable = self.dfs(vertex)
+                reachable = self.dfs(vertex)  #dfs method should return all the vertices touched during the traversal
                 for other_vertex in reachable:
                     other_vertex.component = current_component
                 current_component += 1
@@ -92,5 +101,8 @@ class Graph:
 # graph.add_edge(v3,v6)
 # graph.add_edge(v7,v9)
 
-# print(graph.dfs(v0,v7))
-# print(graph.graph_rec(v0,v7))
+# print(graph.dfs(v0,v9))
+# print(graph.graph_rec(v0,v9))
+
+# graph.find_components()
+# print(graph.components)
