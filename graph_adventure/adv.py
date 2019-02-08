ac6823dbac6b82e3fb3925be263f67f85e821a48
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-07 23:52:43 -0500

    added a Queue class as well

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -72,3 +72,22 @@ class Stack:
     
     def length(self):
         return self.size
+
+class Queue:
+    def __init__(self):
+        self.storage = []
+        self.size = 0
+
+    def enqueue(self, value):
+        self.size += 1
+        self.storage.append(value)
+    
+    def dequeue(self):
+        if len(self.storage) > 0:
+            self.size -= 1
+            return self.storage.pop(0)
+        else:
+            return None
+    
+    def length(self):
+        return self.size
