dd495af522347749a9654d5f838546496a74ad80
Scott Bren <smbren@ncsu.edu>
2019-02-11 16:14:39 -0500

    fixed graph traversal

--- graph_adventure/world.py
+++ graph_adventure/world.py
@@ -11,11 +11,14 @@ class World:
     def loadGraph(self, roomGraph):
         numRooms = len(roomGraph)
         rooms = [None] * numRooms
+        gridSize = 1
         for i in range(0, numRooms):
             x = roomGraph[i][0][0]
+            gridSize = max(gridSize, roomGraph[i][0][0], roomGraph[i][0][1])
             self.rooms[i] = Room(f"Room {i}", f"({roomGraph[i][0][0]},{roomGraph[i][0][1]})",i, roomGraph[i][0][0], roomGraph[i][0][1])
-        gridSize = math.ceil((numRooms * 3) ** 0.5 + 1)
         self.roomGrid = []
+        gridSize += 1
+        self.gridSize = gridSize
         for i in range(0, gridSize):
             self.roomGrid.append([None] * gridSize)
         for roomID in roomGraph:
@@ -30,7 +33,59 @@ class World:
             if 'w' in roomGraph[roomID][1]:
                 self.rooms[roomID].connectRooms('w', self.rooms[roomGraph[roomID][1]['w']])
         self.startingRoom = self.rooms[0]
-
-
-
-
+    def printRooms(self):
+        rotatedRoomGrid = []
+        for i in range(0, len(self.roomGrid)):
+            rotatedRoomGrid.append([None] * len(self.roomGrid))
+        for i in range(len(self.roomGrid)):
+            for j in range(len(self.roomGrid[0])):
+                rotatedRoomGrid[len(self.roomGrid[0]) - j - 1][i] = self.roomGrid[i][j]
+        f = open("map.txt", "w")
+        f.write("#####")
+        print("#####")
+        str = ""
+        for row in rotatedRoomGrid:
+            allNull = True
+            for room in row:
+                if room is not None:
+                    allNull = False
+                    break
+            if allNull:
+                continue
+            # PRINT NORTH CONNECTION ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.n_to is not None:
+                    str += "  |  "
+                else:
+                    str += "     "
+            str += "#\n"
+            # PRINT ROOM ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.w_to is not None:
+                    str += "-"
+                else:
+                    str += " "
+                if room is not None:
+                    str += f"{room.id}".zfill(3)
+                else:
+                    str += "   "
+                if room is not None and room.e_to is not None:
+                    str += "-"
+                else:
+                    str += " "
+            str += "#\n"
+            # PRINT SOUTH CONNECTION ROW
+            str += "#"
+            for room in row:
+                if room is not None and room.s_to is not None:
+                    str += "  |  "
+                else:
+                    str += "     "
+            str += "#\n"
+        f.write(str)
+        f.write("#####")
+        f.close()
+        print(str)
+        print("#####")
