b847d33434c1110e325170a319a395c07b68ec83
benjchand <benchand@gmail.com>
2019-08-16 10:27:54 -0700

    Figured out the new problem

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -135,8 +135,7 @@ for move in traversalPath:
     visited_rooms.add(player.currentRoom)
 
 if len(visited_rooms) == len(roomGraph):
-    print(
-        f"TESTS PASSED: {len(traversalPath)} moves, {len(visited_rooms)} rooms visited")
+    print(f"TESTS PASSED: {len(traversalPath)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(roomGraph) - len(visited_rooms)} unvisited rooms")
