14059cf1dd088080e88a23bf4fb8cfdbef307f01
Quinn Dougherty <quinn.dougherty92@gmail.com>
2019-10-13 16:41:43 -0400

    2234 moves! almost to 2k

--- fourth-approach/adv.py
+++ fourth-approach/adv.py
@@ -8,7 +8,7 @@ from datastructures import Stack, Queue
 
 from random import choice
 from collections import defaultdict, OrderedDict
-from typing import Dict, List
+from typing import Dict, List, Set, Tuple
 # Load world
 world = World()
 
@@ -32,7 +32,7 @@ step_back = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 print()
 # print(build_graph)
 
-def bf_path(graph: Dict[int, Dict[str, int]], starting_room: int) -> Dict[int, List[str]]:
+def bf_paths(graph: Dict[int, Dict[str, int]], starting_room: int) -> Dict[int, List[str]]:
     qq = Queue()
     visited = set()
     qq.enqueue([starting_room])
@@ -42,7 +42,9 @@ def bf_path(graph: Dict[int, Dict[str, int]], starting_room: int) -> Dict[int, L
     while qq.size > 0:
         path: List[int] = qq.dequeue()
         room = path[-1]
+
         if room not in visited:
+
             visited.add(room)
             for move, nextroom in graph[room].items():
                 path_copy = path.copy()
@@ -53,37 +55,91 @@ def bf_path(graph: Dict[int, Dict[str, int]], starting_room: int) -> Dict[int, L
 
     paths = {key: val[1:] for key, val in paths.items()}
     return paths
-    
 
-PATHS_UNORDERED = bf_path(room_graph, 0)
-MAX_PATH = max(len(val) for key, val in PATHS_UNORDERED.items())
-PATHS_SORTED = sorted(PATHS_UNORDERED.items(), key=lambda kv: len(kv[1]))
+def df_paths(graph: Dict[int, Dict[str, int]], starting_room: int) -> Dict[int, List[str]]:
+    ss = Stack()
+    visited = set()
+    ss.push([starting_room])
 
-PATHS = OrderedDict(PATHS_SORTED)
+    paths = {(starting_room,starting_room): [starting_room]}
+
+    while ss.size > 0:
+        path: List[int] = ss.pop()
+        room = path[-1]
+        if room not in visited:
+            visited.add(room)
+            for move, nextroom in graph[room].items():
+                path_copy = path.copy()
+                path_copy.append(nextroom)
+                ss.push(path_copy)
+
+                paths[(starting_room, nextroom)] = paths[(starting_room, room)] + [move]
+
+    paths = {key: val[1:] for key, val in paths.items()}
+    return paths
+
+def get_ord_list_items_from_dict_paths(paths: Dict[int, List[str]]) -> List[Tuple[int, List[str]]]:
+
+    max_path = max(len(path) for key, path in paths.items())
+    paths_sorted = sorted(paths.items(), key=lambda kv: max_path - len(kv[1]))
 
 
+    paths_ = OrderedDict(paths_sorted)
+    return list(paths.items())
 
-REV_PATHS = {(key[1], 0): [step_back[mv] for mv in path[::-1]] for key, path in PATHS.items()}
+PATHS_UNORDERED = bf_paths(room_graph, 0)
+PATHS_ITEMS = get_ord_list_items_from_dict_paths(PATHS_UNORDERED)
 
 visited = set()
 traversal = list()
 curr_room = 0
 visited.add(curr_room)
-while len(visited) != len(roomGraph):
-    for start_end, path in list(PATHS.items())[::-1]:
-        start, end = start_end
-        # print(len(path))
-        if end not in visited: #and len(room_graph[end]) == 1:
+def unvisited() -> Set[int]:
+    global visited
+    global roomGraph
+    return list(set(roomGraph.keys()) - visited)
+# while len(visited) != len(roomGraph):
+while len(unvisited()) > 0:
 
-            # print(end, len(path))
-            for move in path + bf_path(room_graph, end)[(end, start)]: # REV_PATHS[(end, start)]:
-                #print(curr_room, room_graph[curr_room])
-                nextroom = room_graph[curr_room][move]
-                visited.add(nextroom)
-                traversal.append(move)
-                curr_room = nextroom
-    break
+    paths__ = bf_paths(room_graph, curr_room)
+    paths_ = get_ord_list_items_from_dict_paths(paths__)
+    paths = [(key, value) for key, value in paths_ if key[1] in unvisited()]
 
+    start_end, path = paths.pop(0)
+
+    start, end = start_end
+    if end not in visited: #and len(room_graph[end]) == 1:
+
+        midpoint = sorted(unvisited()).pop() # choice(unvisited()
+
+        backtracking = bf_paths(room_graph, end)[(end, midpoint)]
+
+        for move in path + backtracking: # REV_PATHS[(end, start)]:
+            nextroom = room_graph[curr_room][move]
+            visited.add(nextroom)
+            traversal.append(move)
+
+            curr_room = nextroom
+            if len(visited)==len(roomGraph):
+                break
+    #break
+
+#hile len(visited) != len(roomGraph):
+#
+#   Paths = df_paths(room_graph, curr_room)
+#
+#   random = choice(unvisited())
+#   path = Paths[(curr_room, random)]
+#
+#   for move in path + bf_paths(room_graph, random)[(random, curr_room)]:
+#       nextroom = room_graph[curr_room][move]
+#       visited.add(nextroom)
+#       traversal.append(move)
+#       curr_room = nextroom
+#
+#       if len(visited) == len(roomGraph):
+#           break
+#   # break
 
 traversalPath = traversal
 # TRAVERSAL TEST
