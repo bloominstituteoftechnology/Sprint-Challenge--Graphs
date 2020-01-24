240459dddf42df25c50f5db53d573b38a925d150
Adetunji <Ashennaike@gmail.com>
2020-01-24 12:11:14 -0500

    added stack and queue

--- adv.py
+++ adv.py
@@ -1,6 +1,8 @@
 from room import Room
 from player import Player
 from world import World
+from stack import Stack
+from queue import Queue
 
 import random
 from ast import literal_eval
@@ -28,6 +30,7 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+
 
 
 
