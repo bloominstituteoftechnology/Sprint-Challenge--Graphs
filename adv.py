25990dfc87b8faa05ca04a58da77a12639eaacb4
jkaunert <44586402+jkaunert@users.noreply.github.com>
2020-02-26 15:54:47 -0600

    (feat) adds methods to adv.py MVP complete with a
    few trivial bugs.  Tests pass on average ~ 990 moves, 500 rooms visited

--- adv.py
+++ adv.py
@@ -1,62 +1,85 @@
-from room import Room
-from player import Player
+import random
+from helpers.walkingblues import walking_blues
+from ast import literal_eval
+from queue import SimpleQueue
 from world import World
 
-import random
-from ast import literal_eval
+traversal_path = []
+traversal_length_initial_value = 999
+traversal_length = traversal_length_initial_value
+total_rooms_visited = 0
+bfs_queue = SimpleQueue()
+
+
+# walking blues until we hit a dead end
+def depth_first_spelunking(player, graph):
+    global bfs_queue
+    current_room_exits = graph[player.current_room.id]
+    undiscovered_exits = []
+    # Log undiscovered exits in the current room
+    for cardinal in current_room_exits:
+        if current_room_exits[cardinal] is "?":
+            undiscovered_exits.append(cardinal)
+    # Use bfs to retrace path if we hit a dead end
+    if len(undiscovered_exits) is 0:
+        breadcrumbs = breadth_first_spelunking(player, graph)
+        room_on_path = player.current_room.id
+        for next_room in breadcrumbs:
+            for cardinal in graph[room_on_path]:
+                if graph[room_on_path][cardinal] is next_room:
+                    bfs_queue.put_nowait(cardinal)
+                    return
+    # Otherwise choose a random direction
+    else:
+        bfs_queue.put_nowait(
+            undiscovered_exits[random.randint(
+                0, len(
+                    undiscovered_exits
+                ) - 1
+            )
+            ]
+        )
+
+
+# Backtrack to find unexplored paths with bfs
+def breadth_first_spelunking(player, graph):
+    breadcrumbs = SimpleQueue()
+    visited_rooms = set([])
+    breadcrumbs.put_nowait([player.current_room.id])
+    # retrace path as long as there are breadcrumbs in the queue
+    while not breadcrumbs.qsize() is 0:
+        path = breadcrumbs.get_nowait()
+        last_room = path[-1]
+        # Check if we hit an unexplored room
+        if last_room not in visited_rooms:
+            # Log it
+            visited_rooms.add(last_room)
+            # Check exits
+            for room_exit in graph[last_room]:
+                # Return path if we find an unexplored exit
+                if graph[last_room][room_exit] is "?":
+                    return path
+                # Otherwise, keep on truckin'
+                else:
+                    breadcrumb_path = list(path)
+                    breadcrumb_path.append(graph[last_room][room_exit])
+                    breadcrumbs.put_nowait(breadcrumb_path)
+    # when we run out of breadcrumbs in our queue, return an empty list
+    # as we exit the loop since we're back at an unexplored path
+    return list()
+
 
 # Load world
 world = World()
 
-
-# You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
 world.print_rooms()
 
-player = Player(world.starting_room)
-
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
-
-
-
-# TRAVERSAL TEST
-visited_rooms = set()
-player.current_room = world.starting_room
-visited_rooms.add(player.current_room)
-
-for move in traversal_path:
-    player.travel(move)
-    visited_rooms.add(player.current_room)
-
-if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
-else:
-    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
-
-#######
-# UNCOMMENT TO WALK AROUND
-#######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+walking_blues()
