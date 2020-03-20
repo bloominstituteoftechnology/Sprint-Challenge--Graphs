2524086ea25a663f370a6ea0f9f1e0333fe421af
Christopher Pupo <ChrisPupo@Christophers-MacBook-Pro.local>
2020-03-20 15:07:54 -0400

    working on this thing

--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue, Stack
 
 import random
 from ast import literal_eval
@@ -10,24 +11,96 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
+# print(room_graph)
 world.load_graph(room_graph)
+
+
+    
 
 # Print an ASCII map
 world.print_rooms()
 
 player = Player(world.starting_room)
+print()
+# traversal_path = [direction_stack]
+
+# if 'n' in player.current_room.get_exits():
+#     print("true")
+# else: 
+#     print("this isn't working")
+
+# def world_traversal(self, player, room_graph): 
+
+# print('this is what player looks like', player)
+
+def world_traversal(self, player):
+    direction_queue = Queue()
+    visited = set()
+    # starting_room = player
+    direction_queue.enqueue( player )
+
+    while direction_queue.size() < 499: 
+        r = direction_queue.dequeue()
+        if r not in visited:
+            print(r)
+            visited.add(r)
+        for exits in self.player.get_exits(r):
+            direction_queue.enqueue(exits)
+        
+    return world_traversal
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+#     if 'n' in player.current_room.get_exits(): 
+#         # player.travel("n")
+#         # visited.add(player.current_room)
+#         direction_stack.push('n')
+#         player.travel('n')
+#         visited.add(player.current_room)
+#         # print('n' in direction_stack)
+#         print("stack inside of while loop", direction_stack.size())
+#         print(len(visited))
+#     elif 'n' not in player.current_room.get_exits():
+#         pass
+
+            
+
+# print("this is the direction stack",direction_stack)
+
+
+traversal_path = [i for i in direction_queue] 
+
+
+            
+
+# print('All possible exits', player.current_room.get_exits())
+        
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+
+# traversal_path = []
 
 
 
@@ -51,12 +124,18 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
+
+# print("should be seeing something here")
+
+
+
+world_traversal(player=world.starting_room)
