495b7e1fe504b05070e1d911a5ace8d18837c597
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-04-16 14:05:58 -0600

    Q and S just in case i need it

--- adv.py
+++ adv.py
@@ -4,7 +4,31 @@ from world import World
 
 import random
 from ast import literal_eval
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 # Load world
 world = World()
 
