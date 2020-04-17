78cdc2fe6d36ce8fa08e9e8661d38691e2b865df
Jason Loomis <jason@jrloomis.com>
2020-04-17 12:27:47 -0400

    500 rooms, 1000 moves

--- adv.py
+++ adv.py
@@ -23,14 +23,58 @@ room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+# world.print_rooms()
 
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+reverse = {"n": "s", "s": "n", "e": "w", "w": "e"}
 
+# ? first pass (no queue or stack)
+def find_the_cheese(visited_rooms=[]):
+    store_directions = []
+    # print(f"store = {store_directions}")
+
+    # print(f"current room: {player.current_room.id}")
+    # print(f"visited rooms: {visited_rooms}")
+
+    # ? getting directions for current room from get_exits() in room.py
+    for direction in player.current_room.get_exits():
+
+        # ? using travel() from player.py to move player
+        # print(f"dir: {direction}")
+        player.travel(direction)
+
+        # ? check if current room has been visited
+        if player.current_room.id not in visited_rooms:
+            # ? mark current room as visited, track direction
+            # print(f"not visited: {player.current_room.id}")
+            visited_rooms.append(player.current_room.id)
+            store_directions.append(direction)
+
+            # ? set store to current store + updated visited rooms
+            # print(f"store before: {store_directions}")
+            store_directions = store_directions + find_the_cheese(visited_rooms)
+            # print(f"store after: {store_directions}")
+
+            # ? use travel() from player.py to move player, track direction
+            player.travel(reverse[direction])
+            store_directions.append(reverse[direction])
+
+        else:
+            # ? use travel() from player.py to move player in opposite direction
+            # print(f"already visited: {player.current_room.id}")
+            player.travel(reverse[direction])
+
+    # print(f"store = {store_directions}")
+    return store_directions
+
+
+# ? fill traversal_path list
+traversal_path = find_the_cheese()
+# print(f"traversal path: {traversal_path}")
 
 # TRAVERSAL TEST
 visited_rooms = set()
