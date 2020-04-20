ee4940c405c495c933bd173ea8693744c16d0a2d
Ira <ira.d.sanchez@gmail.com>
2020-04-19 21:19:55 -0700

    Add MVP
    - remove graph, didn't realize one was already built in.

--- adv.py
+++ adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from graph import Graph
+from graph import Graph, Stack
 
 import random
 from ast import literal_eval
@@ -13,8 +13,8 @@ world = World()
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+#map_file = "maps/test_loop.txt"
+#map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -28,8 +28,44 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+# traversal_path = ['n', 'n']
 traversal_path = []
+visited = {}
+breadcrumbs = []
+opposite_directions = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
 
+
+# Add the first room to the visited dictionary and get its exits
+visited[player.current_room.id] = player.current_room.get_exits()
+
+# num of visited rooms less than num of rooms in graph
+while len(visited) < len(room_graph):
+    # if we haven't been here yet
+    if player.current_room.id not in visited:
+        # add it to the visited dictionary and add its corresponding exits
+        # get_exits returns a list
+        visited[player.current_room.id] = player.current_room.get_exits()
+        # access the direction just visited and remove it from the unexplored paths as we've just come from that direction
+        last_move = breadcrumbs[-1]
+        visited[player.current_room.id].remove(last_move)
+
+    # no directional options left
+    if len(visited[player.current_room.id]) == 0:
+        # backtrack until there are directions to explore again, take items off as you go
+        last_move = breadcrumbs[-1]
+        breadcrumbs.pop()
+        # add the previous direction to the traversal_path
+        traversal_path.append(last_move)
+        # move the player accordingly
+        player.travel(last_move)
+
+    # if we find a room with unexplored directions, get it ready to explore, and track it in the breadcrumbs
+    else:
+        next_move = visited[player.current_room.id][-1]
+        visited[player.current_room.id].pop()
+        traversal_path.append(next_move)
+        breadcrumbs.append(opposite_directions[next_move])
+        player.travel(next_move)
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
