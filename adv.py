f37bd0cb8707e94528414be29814774625a43979
PercivalN <percival.ngan@gmail.com>
2020-05-15 11:23:38 -0500

    Passed test and MVP, but with 1894 moves

--- adv.py
+++ adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,10 +25,46 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+traversal_path = ['n', 's']
 
+# Set up a direction opposite directions
+opposite_directions = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+
+previous_room = [None]
+room_queue = {}
+visited = {}
+
+def check_direction(roomId):
+    directions = []
+    if 'n' in room_graph[roomId][1].keys():
+        directions.append('n')
+    if 'e' in room_graph[roomId][1].keys():
+        directions.append('e')
+    if 's' in room_graph[roomId][1].keys():
+        directions.append('s')
+    if 'w' in room_graph[roomId][1].keys():
+        directions.append('w')
+    return directions
+
+while len(visited) < len(room_graph):
+    room_id = player.current_room.id
+    if room_id not in room_queue:
+        visited[room_id] = room_id
+        room_queue[room_id] = check_direction(room_id)
+        
+    if len(room_queue[room_id]) < 1:
+        previous_direction = previous_room.pop()
+        traversal_path.append(previous_direction)
+        player.travel(previous_direction)
+        
+    else:
+        next_direction = room_queue[room_id].pop(0)
+        traversal_path.append(next_direction)
+        previous_room.append(opposite_directions[next_direction])
+        player.travel(next_direction)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -51,12 +87,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
