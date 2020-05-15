c91ee07c2860d3f75cacc26d6e73d4a5c1c33f8f
DM <dm@DMs-MacBook-Pro.fios-router.home>
2020-05-15 12:42:14 -0400

    tests passing for mvp

--- adv.py
+++ adv.py
@@ -5,6 +5,25 @@ from world import World
 import random
 from ast import literal_eval
 
+'''
+Instructions from README:
+You are provided with a pre-generated graph consisting of 500 rooms. You are responsible for filling traversal_path with directions that, when walked in order, will visit every room on the map at least once.
+
+You know you are done when you have exactly 500 entries (0-499) in your graph and no '?' in the adjacency dictionaries. To do this, you will need to write a traversal algorithm that logs the path into traversal_path as it walks.
+
+There are a few smaller graphs in the file which you can test your traversal method on before committing to the large graph. You may find these easier to debug.
+
+Start by writing an algorithm that picks a random unexplored direction from the player's current room, travels and logs that direction, then loops. This should cause your player to walk a depth-first traversal. When you reach a dead-end (i.e. a room with no unexplored paths), walk back to the nearest room that does contain an unexplored path.
+
+You can find the path to the shortest unexplored room by using a breadth-first search for a room with a '?' for an exit. If you use the bfs code from the homework, you will need to make a few modifications.
+
+Instead of searching for a target vertex, you are searching for an exit with a '?' as the value. If an exit has been explored, you can put it in your BFS queue like normal.
+
+BFS will return the path as a list of room IDs. You will need to convert this to a list of n/s/e/w directions before you can add it to your traversal path.
+
+If all paths have been explored, you're done!
+'''
+
 # Load world
 world = World()
 
@@ -17,7 +36,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,10 +44,57 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
+# USING RECURSIVE DEPTH FIRST TRAVERSAL
+
+def graph_traversal(current_room, visited=None, directions=None):
+
+    # create set to store visited
+    if visited is None:
+        visited = set()
+
+    # if no directions have been stored (first time graph_traversal is callled)
+    if directions is None:
+        directions = []
+
+    # if all rooms have been visited
+    if len(visited) == len(room_graph):
+        return directions
+
+    # store opposite directions
+    opposite_directions = {"n": "s", "s": "n", "w": "e", "e": "w"}
+
+    # get all exits for current room, travel to selected room
+    for direction in player.current_room.get_exits():
+        player.travel(direction)
+
+    # if room in visited, go back in opposite direction
+        if player.current_room in visited:
+            player.travel(opposite_directions[direction])
+        else:
+            # add room to visited
+            visited.add(player.current_room)
+            # append direction to list of directions
+            directions.append(direction)
+
+            # repeat recursively w/current room
+            graph_traversal(player.current_room, visited, directions)
+
+            # return to previous room
+            player.travel(opposite_directions[direction])
+            # add return to directions traversal
+            directions.append(opposite_directions[direction])
+
+    # return directions for traversing the graph
+    return directions
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
+# traversal_path = []
 
+traversal_path = graph_traversal(player.current_room)
+print(traversal_path)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -41,11 +107,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
