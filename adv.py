8603f089586b97a885169126f62071340f2c6ee9
hyamynl619 <amy.nguyenle619@gmail.com>
2020-07-17 11:22:07 -0400

    finished SC> 1014 moves, 500 rooms

--- adv.py
+++ adv.py
@@ -4,6 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
+import time
 
 # Load world
 world = World()
@@ -28,10 +29,92 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+return_sequence = []
+
+print("STARTING")
+my_room_graph = {}
+visited_rooms = set()
+all_rooms_visited = False
+opposite_directions = {
+    "n": "s",
+    "s": "n",
+    "w": "e",
+    "e": "w"
+}
+
+def add_room_to_graph(room_id, exit_options, prev_r=None, prev_r_direction=None):
+    my_room_graph[room_id] = {}
+    for exit_option in exit_options:
+        if exit_option == prev_r_direction:
+            my_room_graph[room_id][exit_option] = prev_r
+        else:
+            my_room_graph[room_id][exit_option] = "?"
+
+def get_moves(current_exits):
+    moves = []
+    for exit_option in current_exits:
+        if my_room_graph[room_id][exit_option] == "?":
+            moves.append(exit_option)
+    return moves
+
+# add first room to graph
+add_room_to_graph(player.current_room.id, player.current_room.get_exits())
+
+while not all_rooms_visited:
+    current_exits = player.current_room.get_exits()
+    room_id = player.current_room.id
+    print("-"*10)
+    print(f"Current room: {room_id} - {current_exits}")
+    print(f"Curr return moves: {return_sequence}")
+    print(my_room_graph)
+    
+    my_moves = get_moves(current_exits)
+    for exit_option in current_exits:
+        if my_room_graph[room_id][exit_option] == "?":
+            my_moves.append(exit_option)
+    if len(my_moves) == 0:
+        if len(return_sequence) == 0:
+            all_rooms_visited = True
+        else:
+            # return spot until you have moves
+            my_move = return_sequence.pop(-1)
+            print(f"moving {my_move}")
+            traversal_path.append(my_move)
+            player.travel(my_move)
+            # if at starting room, reset return_sequence
+            # may not work for loops not connected to start
+            # maybe could check for loop by seeing ...
+            if player.current_room.id == world.starting_room:
+                return_sequence = []
+
+            # potential_moves = get_moves(player.current_room.get_exits())
+
+            # if player.current_room.id in visited_rooms:
+            #     new_room_moves = get_moves(player.current_room.get_exits())
+            #     if len(new_room_moves) == 0:
+            #         all_rooms_visited = True
+            # potentiall need to see if current room is in visited with no moves
+    else:
+        my_move = my_moves[0]
+        print(f"moving {my_move}")
+        traversal_path.append(my_move)
+        return_sequence.append(opposite_directions[my_move])
+        player.travel(my_move)
+        new_room_id = player.current_room.id
+
+        # add new_room_id to room_id's graph
+        my_room_graph[room_id][my_move] = new_room_id
+        if new_room_id not in visited_rooms:
+            # add new room to visited
+            visited_rooms.add(new_room_id)
+            # add new_room_id to graph
+            add_room_to_graph(new_room_id, player.current_room.get_exits(), room_id, opposite_directions[my_move])
+    print()
+    # time.sleep(.5)
 
 
 
-# TRAVERSAL TEST - DO NOT MODIFY
+# TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
