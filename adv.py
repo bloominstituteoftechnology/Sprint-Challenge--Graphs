7c58c761c2ae082bd518d8eaec1c192e3f171955
Kim Kiamco <kim.kiamco@menlosecurity.com>
2020-08-02 20:40:55 -0700

    completed

--- adv.py
+++ adv.py
@@ -5,18 +5,6 @@ from world import World
 import random
 from ast import literal_eval
 
-class Queue():
-    def __init__(self):
-        self.queue = []
-    def enqueue(self, value):
-        self.queue.append(value)
-    def dequeue(self):
-        if self.size() > 0:
-            return self.queue.pop(0)
-        else:
-            return None
-    def size(self):
-        return len(self.queue)
 
 def random_direction():
     directions = ['n','s','e','w']
@@ -26,51 +14,76 @@ def random_direction():
 
     return directions[random_index]
 
-def get_edges(room):
-    edges = {}
-    print(room)
-    next_rooms = room.get_exits()
 
-    for direction in next_rooms:
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)     
+    def look_behind(self):
+        return self.stack[-1]
 
-        # clear existing edges
-        room = room.get_room_in_direction(direction)
-        edges[direction] = room
+# dictionary to hold inverse directions for backing up part         
+reverse_directions = {'n':'s', 's':'n', 'w':'e', 'e':'w'}
 
-    return edges
+def traverse_world(world):
+    # init player
+    player = Player(world.starting_room)
 
-def create_path(world):
+    traversal_path = []        
 
-    q = Queue()
-    visited = set()
-    q.enqueue(world.rooms[0])
+    # create stack
+    s = Stack() # hold rooms and direction tuples that got us there so can reverse
 
-    while q.size() > 0:
-        # dequeue
-        current_room = q.dequeue()
-        visited.add(current_room)        
-        edges = get_edges(current_room)
+    # vreate vis
+    visited = {} # holds k,v pairs of rooms and directions we've gone so far
+    s.push(value=(player.current_room, None))
 
+    while s.size() > 0:
+        curr = s.look_behind()
+        room, direction_traveled = curr[0], curr[1] 
 
-        #  check if visited
-        if current_room not in visited:
-            # print(current_room)
-            visited.add(current_room)
+        if room.id not in visited: 
+            visited[room.id] = set() # initial set of directions we've gone, use set to avoid duplicates
 
-            # check each exit 
+        if direction_traveled: 
+            visited[room.id].add(direction_traveled) 
 
-            ### map rooms to current directions 
-            
-
-
-        for edge in edges:
-            q.enqueue(edge)
-
-        print(q.queue)
-
-
-            
+        # break if we visit all the rooms
+        if len(visited) == len(room_graph):
+            break
         
+        # DFS
+
+        # get open available directions
+        unexplored_branches = [direction for direction in room.get_exits() if direction not in visited[room.id]]
+
+        # while there is somewhere to go, update everything
+        if unexplored_branches: 
+            random_direction = random.choice(unexplored_branches)
+
+            # update graph
+            visited[room.id].add(random_direction)  
+
+            # push the room we go to and what direction to reverse if dead end   
+            s.push(value=(room.get_room_in_direction(random_direction), reverse_directions[random_direction]))
+
+            # append to path
+            traversal_path.append(random_direction) 
+        
+        else: # back up 
+            print("BACKING UP", direction_traveled)
+            traversal_path.append(direction_traveled)
+            s.pop()
+    
+    return traversal_path
 
 
 # Load world
@@ -79,26 +92,26 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
-world.print_rooms()
+print(world.print_rooms())
 
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
+
+traversal_path = traverse_world(world)
 
 
-create_path(world)
+
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
