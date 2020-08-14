958094a1d7f84a02bd26dcb6b8e8117c80a9c034
nick migel <nickmigel2@gmail.com>
2020-08-14 12:28:10 -0400

    finally found some reference material

--- adv.py
+++ adv.py
@@ -5,16 +5,18 @@ from world import World
 import random
 from ast import literal_eval
 
+from collections import deque
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -29,82 +31,102 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 
 
-q = []
-visited = {}
+def dir_entry(directions_list):
+    new_dir = {
+        "n": None,
+        "e": None,
+        "s": None,
+        "w": None
+    }
+
+    for dir in directions_list:
+        new_dir[dir] = dir
+
+    return new_dir
+
+# search of the visited set starting node
 
 
-def add_to_visited():
-    for exit in player.current_room.get_exits():
-        if visited.get(player.current_room.id):
-            visited[player.current_room.id][exit] = '?'
+def bfs_nearest_open_node(visited, starting_node):
+
+    q = deque()
+    q.append([starting_node])
+
+    q2 = deque()
+    bfs_visited = set()
+
+    while len(q) > 0:
+        if len(q2) <= 0:
+            path = []
+
+        # records path
         else:
-            visited[player.current_room.id] = {exit: '?'}
+            path = q2.popleft()
 
+        cur_node_path = q.popleft()
+        current_node = cur_node_path[-1]
 
-def add_path_to_prev(v, prev_id, cur_id):
-    if v == 'n':
-        visited[cur_id]['s'] = prev_id
+        bfs_visited.add(current_node.id)
 
-    if v == 's':
-        visited[cur_id]['n'] = prev_id
+        if len(current_node.get_exits()) > 0:
+            for next_room in current_node.get_exits():
+                next_node = current_node.get_room_in_direction(next_room)
 
-    if v == 'w':
-        visited[cur_id]['e'] = prev_id
+                if next_node.id not in visited:
+                    return [*path, next_room]
 
-    if v == 'e':
-        visited[cur_id]['w'] = prev_id
+                elif next_node.id not in bfs_visited:
+                    q2.append([*path, next_room])
+                    q.append([*cur_node_path, next_node])
 
+        else:
+            return path
 
-def reverse(v):
-    if v == 'n':
-        return 's'
-
-    if v == 's':
-        return 'n'
-
-    if v == 'w':
-        return 'e'
-
-    if v == 'e':
-        return 'w'
-
-
-prev_id = player.current_room.id
-add_to_visited()
 
 traversal_path = []
-backtrack = []
 
-q.append(random.choice(player.current_room.get_exits()))
-while len(q) > 0:
-    v = q.pop()
-    player.travel(v)
-    backtrack.append(reverse(v))
-    if prev_id is not player.current_room.id:
-        add_to_visited()
+graph = dict()
+visited = set()
 
-    if visited[player.current_room.id].get(v) == '?':
-        visited[prev_id][v] = player.current_room.id
-        add_path_to_prev(v, prev_id, player.current_room.id)
+while len(visited) < len(world.rooms):
+    paths = player.current_room.get_exits()
+    room_id = player.current_room.id
+
+    if room_id not in visited:
+        visited.add(room_id)
+
+    if room_id not in graph:
+        graph[room_id] = dir_entry(paths)
+
+        for next_room in paths:
+            graph[room_id][next_room] = player.current_room.get_room_in_direction(
+                next_room).id
+
+    unexplored_route = None
+
+    for dir in graph[room_id]:
+        if graph[room_id][dir] is not None and graph[room_id][dir] not in visited:
+            unexplored_route = dir
+
+    if unexplored_route is not None:
+        traversal_path.append(unexplored_route)
+        player.travel(unexplored_route)
+
     else:
+        pathfinding = bfs_nearest_open_node(visited, player.current_room)
 
-        print('this ran')
-        player.travel(backtrack[-1])
-        backtrack.pop()
+        if pathfinding is not None:
+            for dir in pathfinding:
+                traversal_path.append(dir)
+                player.travel(dir)
+                visited.add(player.current_room.id)
 
-        for next_v in visited[player.current_room.id]:
-            if visited[player.current_room.id][next_v] == '?':
-                q.append(next_v)
 
-    prev_id = player.current_room.id
-    traversal_path.append(v)
-
-    print(backtrack)
-print(visited, player.current_room.id)
-# TRAVERSAL TEST - DO NOT MODIFY
+# TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+
 for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
@@ -115,9 +137,8 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-    print(traversal_path)
 
-
+#
 #######
 # UNCOMMENT TO WALK AROUND
 #######
