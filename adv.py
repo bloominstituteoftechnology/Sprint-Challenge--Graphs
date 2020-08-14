3b81f68e115920e37d45b432dc589dd85a8ed5d3
juarezfrench <markedfrench@gmail.com>
2020-08-14 12:51:30 -0500

    Accidentally deleted traversal test - added in back in...  ;-D

--- adv.py
+++ adv.py
@@ -153,6 +153,22 @@ while len(world.rooms) > len(visited):
         # remove that room from rooms_to_visit
         rooms_to_visit.remove(player.current_room.id)
 
+# TRAVERSAL TEST - DO NOT MODIFY
+visited_rooms = set()
+player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
+
+for move in traversal_path:
+    player.travel(move)
+    visited_rooms.add(player.current_room)
+
+if len(visited_rooms) == len(room_graph):
+    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+else:
+    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+
+
 #######
 # UNCOMMENT TO WALK AROUND
 #######
