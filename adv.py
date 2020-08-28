f5d53d9b96c67caaab64589dc186de6ee4e6d99a
stevekutz <stkutz@gmail.com>
2020-08-27 19:29:09 -0500

    able to track avail room dir with tracking list with walking!

--- adv.py
+++ adv.py
@@ -12,8 +12,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -22,16 +22,20 @@ map_file = "maps/test_line.txt"
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
-# create graph 
-g = Graph()
-g.build_world(room_graph)
-
-
+# print(f'%%%  room_graph[0]  {room_graph[0]} ')
+# print(f'\t  room_graph[0][-1]  {room_graph[0][-1]}   ')
 
 # Print an ASCII map
 world.print_rooms()
 
 player = Player(world.starting_room)
+
+# create graph 
+g = Graph()
+g.build_graph(room_graph)
+print(f' g rooms >>  {g.rooms}')
+# g rooms >>  {0: {'n': '?'}, 1: {'s': '?', 'n': '?'}, 2: {'s': '?'}}
+
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
@@ -39,7 +43,42 @@ traversal_path = []
 
 
 print(f' current info {player.current_room} ')
+print(f' current info id  {player.current_room.id} ')
 print(f' exits:  {player.current_room.get_exits()}')
+
+
+
+for room in g.rooms:
+    print(f'  room: {room} >>  {g.rooms[room]}  ')
+
+
+# set of visited rooms 
+visited = set()
+
+
+# TEST starting room 0
+# room:0   g_room_dir {'n': '?', 's': '?', 'e': '?', 'w': '?'}
+
+# MOVE 'n'
+# player.travel('n')
+# room:1   g_room_dir {'s': '?', 'n': '?'}       list_room_dir ['?', '?'] 
+
+
+while len(visited) < len(room_graph):
+    
+    # !!!!!   3
+    g_room_dir = g.rooms[player.current_room.id]
+    list_room_dir = list(g_room_dir.values())
+
+    print(f' room:{player.current_room.id}   g_room_dir {g_room_dir}       list_room_dir {list_room_dir} ')
+    
+
+    if  '?'  in list_room_dir:
+        print(f' Not Done')
+        break
+
+
+
 
 
 
--- utils.py
+++ utils.py
@@ -28,7 +28,20 @@ class Graph:
     def __init__(self):
         self.rooms = {}
 
-    def build_world(self, room_graph):
+    # add each room to graph
+    def build_graph(self, room_graph):
         for room in room_graph:
             if room not in self.rooms:
                 self.rooms[room] = set()
+
+        # add direction info to each room
+        for room in room_graph:
+            for direction in room_graph[room][-1]:
+                # print(f' dir {direction}')
+                self.rooms[room] = room_graph[room][-1]
+                room_info = self.rooms[room]
+                dir_info = self.rooms[room][direction]
+                # print(f' room: {room} self.room[room] >> {room_info}  self.rooms[room][direction] {dir_info}')    
+
+                # ONLY for avail direction, change to '?'
+                self.rooms[room][direction] = '?'
