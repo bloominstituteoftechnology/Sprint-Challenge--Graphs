c82fe769bf4a8f18c14bbcc83318ce1aaa00746a
Dlray89 <drayjr89@gmail.com>
2020-08-31 19:54:50 -0400

    update: added my traversal path in player.py file instead of the adv.py. 997 moves from 500000

--- adv.py
+++ adv.py
@@ -2,8 +2,6 @@ from room import Room
 from player import Player
 from world import World
 
-from utils import Stack
-
 import random
 from ast import literal_eval
 
@@ -12,11 +10,13 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-#map_file = "maps/test_line.txt"
-#map_file = "maps/test_cross.txt"
-#map_file = "maps/test_loop.txt"
-#map_file = "maps/test_loop_fork.txt"
+# map_file = "./projects/adventure/maps/test_cross.txt"
+# map_file = "./projects/adventure/maps/test_loop.txt"
+# map_file = "./projects/adventure/maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
+
+import os
+print(os.getcwd())
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -25,69 +25,15 @@ world.load_graph(room_graph)
 # Print an ASCII map
 world.print_rooms()
 
-player = Player(world.starting_room)
+player = Player(world.starting_room, len(world.rooms))
+player.start_maze()
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
-
-def prevPath(dir):
-    if dir == 'n':
-        return 's'
-    elif dir == 's':
-        return 'n'
-    elif dir == 'e':
-        return 'w'
-    elif dir == 'w':
-        return 'e'
-    else:
-        print('You enter an invaild response')
-
-def roomTraversal(starting_room):
-    stack = Stack()
-    visited = set()
-
-    # see where the player is starting from
-    sstarting_room = player.current_room
-
-    # loop through all exits until all rooms all visited
-    while len(visited) < len(world.rooms):
-        #create a path to record prev path traveled
-        path = []
-
-        for room_exit in player.current_room.get_exits():
-
-            if room_exit is not None:
-                if player.current_room.get_room_in_direction(exit) not in visited:
-
-                    path.append(room_exit)
-                    print('path', path)
-        visited.add(player.current_room)
-
-        if len(path) != 0:
-
-            move = random.randint(0, len(path) - 1)
-            stack.push(path[move])
-            player.travel(path[move])
-
-            traversal_path.append(path[move])
-        else:
-
-            last = stack.pop()
-
-            player.travel(prevPath(last))
-            traversal_path.append(prevPath(last))
+traversal_path = player.traversal_path.copy()
 
 
-roomTraversal(0)
-
-
-
-                          
-
-
-
-# TRAVERSAL TEST - DO NOT MODIFY
+# TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
@@ -103,16 +49,15 @@ else:
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- player.py
+++ player.py
@@ -1,11 +1,59 @@
+from utils import Queue, Stack
+
 class Player:
-    def __init__(self, starting_room):
+    def __init__(self, starting_room, num_rooms):
         self.current_room = starting_room
+        self.seen = {starting_room.id}
+        self.num_rooms = num_rooms
+        self.traversal_path = []
+        
     def travel(self, direction, show_rooms = False):
         next_room = self.current_room.get_room_in_direction(direction)
         if next_room is not None:
             self.current_room = next_room
+            self.traversal_path.append(direction)
+            self.seen.add(self.current_room.id)
             if (show_rooms):
                 next_room.print_room_description(self)
-        else:
-            print("You cannot move in that direction.")
+
+    def find_room(self):
+        # implement bfs to find path to nearest unseen room
+        
+        # def bfs(self, starting_vertex, destination_vertex):
+        # """
+        # Return a list containing the shortest path from
+        # starting_vertex to destination_vertex in
+        # breath-first order.
+        # """
+
+        # initialize queue with starting vertex
+        queue = Queue()
+        queue.enqueue((self.current_room, []))
+        
+        # set to keep track of vertexes already seen
+        visited = set()
+
+        # while queue is not empty
+        while queue.size() > 0:
+            # get path and vertex
+            room, path = queue.dequeue()
+            # if room has not been seen, return path
+            if room.id not in self.seen:
+                return path
+            # else, add vertex to visited
+            elif room.id not in visited:
+                visited.add(room.id)      
+                # and add paths to the queue for each edge
+                for exit in room.get_exits():
+                    path_copy = path.copy()
+                    path_copy.append(exit)
+                    queue.enqueue((room.get_room_in_direction(exit), path_copy))
+        
+        print('Room not found')
+
+    def start_maze(self):
+        while len(self.seen)<self.num_rooms:
+            path = self.find_room()
+            for direction in path:
+                self.travel(direction)
+                print(direction, self.current_room.id)
