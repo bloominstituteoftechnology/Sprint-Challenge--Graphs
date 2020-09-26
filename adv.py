e2893c41d1c4eea09a1a058712b6c1014b1602f3
Sal B Amer <iosdevsalman@gmail.com>
2020-09-25 20:13:13 -0700

    fixed import error working

--- adv.py
+++ adv.py
@@ -8,14 +8,14 @@ from ast import literal_eval
 # Load world
 world = World()
 
-
+# tested main_maze
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
-
+#note  using maps/main_maze.txt was resulting in constant errors 
 map_file = "/Users/lambdacatalina/Desktop/LAMBDA-SCHOOL/COMPUTER SCIENCE/Unit 6 - Graphs/Sprint-Challenge--Graphs/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -47,7 +47,7 @@ while len(visitedSoFar) < len(room_graph) - 1:
 
         else:
             continue
-    
+
     while len(visitedSoFar[player.current_room.id]) == 0:
         previous_path = reversed_transversal.pop()
         traversal_path.append(previous_path)
