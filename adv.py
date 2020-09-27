1f3e9d24d55b5494362f83d870a6d9a1e9d6ae7c
Joel <joelgroomer@gmail.com>
2020-09-27 13:37:37 -0700

    MVP but not quite stretch? Lowest score I've gotten is 978.

--- adv.py
+++ adv.py
@@ -27,6 +27,14 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+traversal_path = []
+rooms = [None] * len(room_graph)
+my_graph = {}
+visited_rooms = set()
+paths_taken = {}
+
+
 def opposite_path(chosen_path):
     if chosen_path == 'n':
         return 's'
@@ -39,6 +47,7 @@ def opposite_path(chosen_path):
     else:
         return None
 
+
 def prep_room(room_num):
     my_graph[room_num] = {'n': '?', 'e': '?', 's': '?', 'w': '?'}
     if 'n' not in rooms[room_num].keys():
@@ -50,31 +59,71 @@ def prep_room(room_num):
     if 'w' not in rooms[room_num].keys():
         my_graph[room_num]['w'] = None
 
-traversal_path = []
-rooms = [None] * len(room_graph)
-my_graph = {}
-visited_rooms = set()
-paths_taken = {}
+
+def bfs(this_room):
+    """
+    Adds steps to the traversal_path containing the shortest path from
+    start room to a path that hasn't been explored yet. Returns the
+    room number of that path.
+    """
+
+    explored = []
+    q = [[('x', this_room)]]
+    while q:
+        path = q.pop(0)
+        this_room = path[-1][1]
+        if this_room not in explored:
+            directions = [k for k in my_graph[this_room].keys(
+            ) if my_graph[this_room][k] is not None]
+            # print(f'exploring {this_room}: {directions}')
+            for direction in directions:
+                if my_graph[this_room][direction] == '?':
+                    for d, _ in path:
+                        if d != 'x':    # x indicates starting room
+                            traversal_path.append(d)
+                            # print(f'adding dir: {d}')
+                    # print(f'returning {this_room}')
+                    return this_room
+                else:
+                    new_path = list(path)
+                    if my_graph[this_room][direction] not in explored:
+                        new_path.append(
+                            (direction, my_graph[this_room][direction]))
+                        # print(f'appending {new_path}')
+                        q.append(new_path)
+            explored.append(this_room)
+    raise Exception('Unable to find a way to an unexplored path!')
+
 
 for i in range(len(room_graph)):
     rooms[i] = room_graph[i][1]
 
 cur_room = 0
-while len(visited_rooms) < len(rooms):
+chosen_path = None
+while True:
     visited_rooms.add(cur_room)
     if cur_room not in my_graph:
         prep_room(cur_room)
-    print(cur_room, end=" ")
+    # print(cur_room, end=" ")
 
-    paths = [k for k in my_graph[cur_room].keys() if my_graph[cur_room][k] is not None]
+    paths = [k for k in my_graph[cur_room].keys() if my_graph[cur_room]
+             [k] is not None]
     new_paths = [p for p in paths if my_graph[cur_room][p] == '?']
 
     if len(new_paths) > 0:
+        # pick only from paths that havne't already been traveled
         chosen_path = random.choice(new_paths)
     else:
-        chosen_path = random.choice(paths)
-    
-    print(chosen_path, end=" ")
+        if len(visited_rooms) == len(rooms):
+            break
+
+        # BFS for shortest path to an unexplored path
+        cur_room = bfs(cur_room)
+        # print(f'bfs to {cur_room}', end=" ")
+        # print(f'(vis: {(visited_rooms)} rooms: {len(rooms)})', end=" ")
+        continue
+
+    # print(chosen_path, end=" ")
     traversal_path.append(chosen_path)
     next_room = rooms[cur_room][chosen_path]
     my_graph[cur_room][chosen_path] = next_room
@@ -83,8 +132,6 @@ while len(visited_rooms) < len(rooms):
     my_graph[next_room][opposite_path(chosen_path)] = cur_room
 
     cur_room = next_room
-
-    
 
 
 # while len(visited_rooms) < 500:
