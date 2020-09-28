f1a41266179e95256e5508aaed4d9d76ad83998f
Eric <eric@erics-mbp.localdomain>
2020-09-27 20:37:09 -0500

    MVP

--- adv.py
+++ adv.py
@@ -17,7 +17,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -27,8 +27,42 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
+
 traversal_path = []
 
+
+def reverse_direction(direction):
+    if(direction == 'n'):
+        return_direction = 's'
+
+    if(direction == 's'):
+        return_direction = 'n'
+
+    if(direction == 'w'):
+        return_direction = 'e'
+
+    if(direction == 'e'):
+        return_direction = 'w'
+
+    return return_direction
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -36,16 +70,51 @@ visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
 
-for move in traversal_path:
-    player.travel(move)
+moves_made = Stack()
+
+while len(visited_rooms) < len(world.rooms):
+    available_exits = player.current_room.get_exits()
+    available_directions = [exit for exit in available_exits if player.current_room.get_room_in_direction(
+        exit) not in visited_rooms]
+
+    # add current room to visited set()
     visited_rooms.add(player.current_room)
 
+    if len(available_directions):
+        # pick a random direction and push it to the stack[]
+        randomDirection = random.randint(0, len(available_directions) - 1)
+        moves_made.push(available_directions[randomDirection])
+        # move the player in chosen direction and append move to traversal_path[]
+        player.travel(available_directions[randomDirection])
+        traversal_path.append(available_directions[randomDirection])
+    else:
+        # if there are no available directions, get the latest move made
+        latestMove = moves_made.pop()
+        # reverse the last move and append to traversal_path[]
+        player.travel(reverse_direction(latestMove))
+        traversal_path.append(reverse_direction(latestMove))
+
+[player.travel(move) and visited_rooms.add(player.current_room)
+ for move in traversal_path]
+
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
+
+# for move in traversal_path:
+#     player.travel(move)
+#     visited_rooms.add(player.current_room)
+
+# if len(visited_rooms) == len(room_graph):
+#     print(
+#         f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+# else:
+#     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+#     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
 #######
