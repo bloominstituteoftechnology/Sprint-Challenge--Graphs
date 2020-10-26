df85b20a8f1b95cee650050c4146980bd4539959
chiragthesia <harithesia@gmail.com>
2020-10-26 18:00:00 -0500

    fixed error

--- adv.py
+++ adv.py
@@ -5,6 +5,7 @@ from world import World
 import random
 from ast import literal_eval
 
+
 # Load world
 world = World()
 
@@ -17,7 +18,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -29,32 +30,32 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
-#keep track 
+# keep track of "reverse" directions
+# use this to return to a room with valid moves
 backtrack = []
-reversed_direction={"n": "s", "s":"n", "e":"w", "w":"e"}
+reversed_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
+# instantiate a set to keep track of visited rooms
 visited = set()
 
+# while there are still unvisited rooms
 while len(visited) < len(room_graph):
-    next_move= None
-    
-    #for all the exits in the room
+    next_move = None
+    # for each exit(neighbor) in the room expressed by (n,s,e,w)
     for exit in player.current_room.get_exits():
-        #if not visited then next move. 
         if player.current_room.get_room_in_direction(exit) not in visited:
             next_move = exit
             break
-        #if viable move 
-        if next_move is not None: 
-            traversal_path.append(next_move)
-            backtrack.append(reversed_direction[next_move])
-            player.travel(next_move)
-            visited.add(player.current_room)
-        else: 
-            next_move = backtrack.pop()
-            traversal_path.append(next_move)
-            player.travel(next_move)
-            
+    if next_move is not None:
+        traversal_path.append(next_move)
+        backtrack.append(reversed_directions[next_move])
+        player.travel(next_move)
+        visited.add(player.current_room)
+
+    else:
+        next_move = backtrack.pop()
+        traversal_path.append(next_move)
+        player.travel(next_move)
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
@@ -66,14 +67,12 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
-
-def travelers_path(direction):
-    return None
 #######
 # UNCOMMENT TO WALK AROUND
 #######
