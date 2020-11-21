76f96139f74cf1e3a34c8b42c63d442a510e9559
Enayatullah <enayatullahna@gmail.com>
2020-11-20 19:40:54 -0800

    Main path

--- adv.py
+++ adv.py
@@ -5,33 +5,27 @@ from world import World
 import random
 from ast import literal_eval
 
-# Load world
 world = World()
 
 
-# You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
 map_file = "maps/main_maze.txt"
 
-# Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
-# Print an ASCII map
 world.print_rooms()
 
 player = Player(world.starting_room)
 
-# Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
 
+visited = {}
+
+my_path = []
+
+back_direction = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
 
-# TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
