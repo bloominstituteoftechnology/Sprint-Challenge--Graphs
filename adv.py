8dbbc81a06d8fc7c5bd215dd32715a2166d6cfaf
Veto Ramirez <vetorobert3@gmail.com>
2020-11-21 11:22:12 -0800

    Working out the code

--- adv.py
+++ adv.py
@@ -28,7 +28,42 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = ['n', 's', 'w', 'e']
+
 # Do BFS to find path then DFT to move the player
+def dftr_maze(current_room, visited=None):
+    directions = []
+
+    # Creating a set to hold visited rooms if visited in None
+    if visited == None:
+        visited = set()
+
+    # Finding exits for current_room using player from player and get_exits from room
+    for move in player.current_room.get_exits():
+        # making moves using travel from player
+        player.travel(move)
+
+        # reverse_move if already visited to find new path
+        if player.current_room in visited:
+            player.travel(reverse_move[move])
+        # if its a new room, do the following
+        else:
+            # add to visited stack
+            visited.add(player.current_room)
+            # adding this move to 'direction'
+            directions.append(move)
+            # recursive: repeating loop and adding directions ('\' is an escape character in Python)
+            directions = directions + \
+                dftr_maze(player.current_room, visited)
+            # go_back to previous room
+            player.travel(reverse_move[move])
+            # appending this move to 'directions'
+            directions.append(reverse_move[move])
+
+    return directions
+
+    # fill this out with directions to walk
+    traversal_path = dftr_maze(player.current_room)
+    # print(dftr_maze)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
