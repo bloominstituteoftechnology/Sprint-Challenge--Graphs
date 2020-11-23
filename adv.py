b049b07ef29de7a570246ed50eeb949f4b43580c
Matt Martindale <martindalematt18@yahoo.com>
2020-11-22 18:57:07 -0700

    Started project

--- adv.py
+++ adv.py
@@ -29,6 +29,12 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+def traverse_graph(current_room):
+    # Create traversal graph
+    print(f"Current: {current_room}")
+    return ["s", "w"]
+
+traversal_path = traverse_graph(player.current_room)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -51,12 +57,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
